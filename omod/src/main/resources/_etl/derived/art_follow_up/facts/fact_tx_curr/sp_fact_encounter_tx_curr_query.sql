9478f96e92af4236012ac7b776b4c87a9e746aea
samuelabebayehu <saa2280@cumc.columbia.edu>
2024-08-05 09:27:47 +0300

    update coarse and fine age group data on art followup insert

--- omod/src/main/resources/_etl/derived/art_follow_up/facts/fact_tx_curr/sp_fact_encounter_tx_curr_query.sql
+++ omod/src/main/resources/_etl/derived/art_follow_up/facts/fact_tx_curr/sp_fact_encounter_tx_curr_query.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS sp_fact_encounter_art_follow_up_tx_curr_query;
 
 CREATE PROCEDURE sp_fact_encounter_art_follow_up_tx_curr_query(IN REPORT_END_DATE DATE)
 BEGIN
-    WITH latest_follow_up AS (
+    WITH LatestFollowUp AS (
         SELECT
             client_id,
             hiv_confirmed_date,
@@ -27,44 +27,41 @@ BEGIN
             viral_load_test_status,
             ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY followup_date DESC) AS rn_desc
         FROM mamba_fact_art_follow_up
-        WHERE
-            followup_date <= CAST(REPORT_END_DATE AS DATE)
+        WHERE followup_date <= CAST(REPORT_END_DATE AS DATE)
           AND art_start_date <= CAST(REPORT_END_DATE AS DATE)
           AND regimen IS NOT NULL
     )
     SELECT
-        subquery.client_id,
-        dim_art_follow_up.patient_name,
-        dim_art_follow_up.mrn,
-        dim_art_follow_up.uan,
-        dim_art_follow_up.current_age,
-        fn_mamba_age_calculator(dim_art_follow_up.date_of_birth, subquery.enrollment_date) AS age_at_enrollment,
-        dim_art_follow_up.sex,
-        dim_art_follow_up.mobile_no,
-        subquery.hiv_confirmed_date,
-        subquery.art_start_date,
-        subquery.followup_date,
-        subquery.weight_in_kg,
-        subquery.pregnancy_status,
-        subquery.regimen,
-        subquery.arv_dose_days,
-        subquery.follow_up_status,
-        subquery.anitiretroviral_adherence_level,
-        subquery.next_visit_date,
-        subquery.dsd_category,
-        subquery.tpt_start_date,
-        subquery.tpt_completed_date,
-        subquery.tpt_discontinued_date,
-        subquery.tuberculosis_treatment_end_date,
-        subquery.date_viral_load_results_received,
-        subquery.viral_load_test_status,
-        NOW() AS query_run_time
-    FROM latest_follow_up AS subquery
-             JOIN mamba_dim_client_art_follow_up AS dim_art_follow_up
-                  ON subquery.client_id = dim_art_follow_up.client_id
-    WHERE subquery.rn_desc = 1
-      AND subquery.follow_up_status = 'Alive'
-    GROUP BY subquery.client_id;
+        lf.client_id,
+        dim.patient_name,
+        dim.mrn,
+        dim.uan,
+        dim.current_age,
+        fn_mamba_age_calculator(dim.date_of_birth, lf.enrollment_date) AS age_at_enrollment,
+        dim.sex,
+        dim.mobile_no,
+        lf.hiv_confirmed_date,
+        lf.art_start_date,
+        lf.followup_date,
+        lf.weight_in_kg,
+        lf.pregnancy_status,
+        lf.regimen,
+        lf.arv_dose_days,
+        lf.follow_up_status,
+        lf.anitiretroviral_adherence_level,
+        lf.next_visit_date,
+        lf.dsd_category,
+        lf.tpt_start_date,
+        lf.tpt_completed_date,
+        lf.tpt_discontinued_date,
+        lf.tuberculosis_treatment_end_date,
+        lf.date_viral_load_results_received,
+        lf.viral_load_test_status,
+        NOW()
+    FROM LatestFollowUp lf
+             JOIN mamba_dim_client_art_follow_up dim ON lf.client_id = dim.client_id
+    WHERE lf.rn_desc = 1
+      AND lf.follow_up_status = 'Alive';
 END //
 
 DELIMITER ;
