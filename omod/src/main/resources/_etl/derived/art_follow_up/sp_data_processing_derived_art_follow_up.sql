21a77d279b6edde8861ce2fa130cea7a7a1b479d
betsegaw-d <bda2117@cumc.columbia.edu>
2024-09-05 07:56:40 +0000

    tx curr analysis work in progress

--- omod/src/main/resources/_etl/derived/art_follow_up/sp_data_processing_derived_art_follow_up.sql
+++ omod/src/main/resources/_etl/derived/art_follow_up/sp_data_processing_derived_art_follow_up.sql
@@ -1,7 +1,14 @@
 -- $BEGIN
 CALL sp_dim_client_art_follow_up();
+
 CALL sp_fact_encounter_art_follow_up();
+
 CALL sp_fact_encounter_art_first_follow_up();
+
 CALL sp_fact_encounter_art_latest_follow_up();
+
 CALL sp_fact_encounter_tx_new();
+
+CALL sp_fact_encounter_art_follow_up_tx_curr_analytics_query();
+
 -- $END
--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -1,10 +1,11 @@
--- $BEGIN
--- add base folder SP here --
+DELIMITER //
 
--- Flatten OpenMRS Observational Data
-CALL sp_mamba_data_processing_flatten();
+DROP PROCEDURE IF EXISTS sp_mamba_data_processing_etl;
+
+CREATE PROCEDURE sp_mamba_data_processing_etl(IN etl_incremental_mode INT)
 
 -- Add the implementation-specific ETL processes here
+CALL sp_mamba_drop_all_derived_tables();
 CALL sp_data_processing_derived_art_follow_up();
 CALL sp_data_processing_derived_transfer_in();
 CALL sp_data_processing_derived_transfer_out();
