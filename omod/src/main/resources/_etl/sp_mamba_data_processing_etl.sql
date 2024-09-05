6558c704b3d01852ba238e920290a2af9050a3ea
samuelabebayehu <saa2280@cumc.columbia.edu>
2024-09-05 13:33:55 +0300

    updated new sp_mamba_data_processing_etl.sql config

--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -4,9 +4,15 @@ DROP PROCEDURE IF EXISTS sp_mamba_data_processing_etl;
 
 CREATE PROCEDURE sp_mamba_data_processing_etl(IN etl_incremental_mode INT)
 
--- Add the implementation-specific ETL processes here
-CALL sp_mamba_drop_all_derived_tables();
-CALL sp_data_processing_derived_art_follow_up();
-CALL sp_data_processing_derived_transfer_in();
-CALL sp_data_processing_derived_transfer_out();
--- $END
+BEGIN
+    -- add base folder SP here if any --
+
+    -- Call the implementer ETL process
+    CALL sp_mamba_drop_all_derived_tables();
+    CALL sp_data_processing_derived_art_follow_up();
+    CALL sp_data_processing_derived_transfer_in();
+    CALL sp_data_processing_derived_transfer_out();
+
+END //
+
+DELIMITER ;
