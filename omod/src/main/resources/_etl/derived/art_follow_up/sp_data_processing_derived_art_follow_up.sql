33207b2238de81101c91e3398bda07526fcd209a
samuelabebayehu <saa2280@cumc.columbia.edu>
2024-11-01 09:38:32 +0300

    cleanup unnecessary queries

--- omod/src/main/resources/_etl/derived/art_follow_up/sp_data_processing_derived_art_follow_up.sql
+++ omod/src/main/resources/_etl/derived/art_follow_up/sp_data_processing_derived_art_follow_up.sql
@@ -1,9 +1,4 @@
 -- $BEGIN
 CALL sp_dim_client_art_follow_up();
 CALL sp_fact_encounter_art_follow_up();
-CALL sp_fact_encounter_art_first_follow_up();
-CALL sp_fact_encounter_art_latest_follow_up();
-CALL sp_fact_encounter_tx_new();
-
-
 -- $END
