76e8689c47201bd42c9c27cf7ae99e152d87702b
samuel <saa2280@cumc.columbia.edu>
2024-11-12 20:09:18 +0300

    Delete derived tables for Incremental update

--- omod/src/main/resources/_etl/derived/sp_mamba_drop_all_derived_tables.sql
+++ omod/src/main/resources/_etl/derived/sp_mamba_drop_all_derived_tables.sql
@@ -11,7 +11,9 @@ BEGIN
     DROP TABLE IF EXISTS mamba_fact_client_tranfer_in;
     DROP TABLE IF EXISTS mamba_fact_client_tranfer_out;
     DROP TABLE IF EXISTS mamba_dim_client_art_follow_up;
-    DROP TABLE IF EXISTS sp_fact_location_tag;
-    DROP TABLE IF EXISTS sp_fact_location_tag_map;
+    DROP TABLE IF EXISTS mamba_fact_location_attribute;
+    DROP TABLE IF EXISTS mamba_fact_location_attribute_type;
+    DROP TABLE IF EXISTS mamba_fact_location_tag;
+    DROP TABLE IF EXISTS mamba_fact_location_tag_map;
 END //
 DELIMITER ;
