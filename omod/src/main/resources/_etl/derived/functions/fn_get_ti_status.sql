3989e6df7764b0cbd584e6da79eec171147ee89a
samuelabebayehu <saa2280@cumc.columbia.edu>
2024-11-14 12:59:17 +0300

    fixed get_ti_function art_start_date source

--- omod/src/main/resources/_etl/derived/functions/fn_get_ti_status.sql
+++ omod/src/main/resources/_etl/derived/functions/fn_get_ti_status.sql
@@ -2,36 +2,34 @@ DROP FUNCTION IF EXISTS fn_get_ti_status;
 
 DELIMITER //
 
-CREATE FUNCTION fn_get_ti_status(TI_CLIENT_ID INT, START_DATE DATE,END_DATE DATE) RETURNS NVARCHAR(10)
-                                                                                                  DETERMINISTIC
+CREATE FUNCTION fn_get_ti_status(TI_CLIENT_ID INT, START_DATE DATE, END_DATE DATE) RETURNS NVARCHAR(10)
+    DETERMINISTIC
 BEGIN
-    DECLARE TI_STATUS NVARCHAR(10);
+
     DECLARE TI_RESULT NVARCHAR(10);
 
-WITH TIFollowUp AS (
-    SELECT follow_up.client_id, MIN(follow_up_date_followup_) AS FollowupDate
-    FROM mamba_flat_encounter_follow_up follow_up
-             JOIN mamba_flat_encounter_follow_up_1 follow_up_1 ON follow_up.encounter_id = follow_up_1.encounter_id
-             JOIN mamba_flat_encounter_follow_up_2 follow_up_2 ON follow_up.encounter_id = follow_up_2.encounter_id
-             JOIN mamba_flat_encounter_follow_up_3 follow_up_3 ON follow_up.encounter_id = follow_up_3.encounter_id
-    WHERE follow_up_status IS NOT NULL
-      AND art_antiretroviral_start_date IS NOT NULL
-      AND transferred_in_check_this_for_all_t IS NOT NULL
-      AND follow_up.client_id = TI_CLIENT_ID
-    GROUP BY follow_up.client_id
-)
-SELECT * INTO TI_STATUS
-FROM TIFollowUp
-WHERE FollowupDate BETWEEN START_DATE AND END_DATE;
-
-
-IF TI_STATUS IS NULL THEN
-        SET TI_RESULT = 'NTI';
-ELSE
+    -- Using EXISTS to check for a valid follow-up date within the specified range
+    IF EXISTS (SELECT 1
+               FROM mamba_flat_encounter_follow_up follow_up
+                        JOIN mamba_flat_encounter_follow_up_1 follow_up_1
+                             ON follow_up.encounter_id = follow_up_1.encounter_id
+                        JOIN mamba_flat_encounter_follow_up_2 follow_up_2
+                             ON follow_up.encounter_id = follow_up_2.encounter_id
+                        JOIN mamba_flat_encounter_follow_up_3 follow_up_3
+                             ON follow_up.encounter_id = follow_up_3.encounter_id
+               WHERE follow_up_status IS NOT NULL
+                 AND art_antiretroviral_start_date IS NOT NULL
+                 AND transferred_in_check_this_for_all_t IS NOT NULL
+                 AND follow_up.client_id = TI_CLIENT_ID
+                 AND follow_up_date_followup_ BETWEEN START_DATE AND END_DATE) THEN
         SET TI_RESULT = 'TI';
-END IF;
+    ELSE
+        SET TI_RESULT = 'NTI';
+    END IF;
 
-RETURN TI_RESULT;
+    RETURN TI_RESULT;
+
+
 END //
 
 DELIMITER ;
