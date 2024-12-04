b51b77bfdeec9c84d4d08184e16bc14e07d7c2d4
samuelabebayehu <saa2280@cumc.columbia.edu>
2024-12-04 14:38:04 +0300

    dim client updated

--- omod/src/main/resources/_etl/derived/client/dimensions/dim_client/sp_dim_client_create.sql
+++ omod/src/main/resources/_etl/derived/client/dimensions/dim_client/sp_dim_client_create.sql
@@ -1,21 +1,27 @@
 -- $BEGIN
 CREATE TABLE IF NOT EXISTS mamba_dim_client
 (
-    id                INT AUTO_INCREMENT,
-    client_id         INT           NOT NULL,
-    patient_name      NVARCHAR(255) NULL,
-    mrn               NVARCHAR(50)  NULL,
-    uan               NVARCHAR(50)  NULL,
+    client_id         int                                not null
+        primary key,
+    patient_name      VARCHAR(255) NULL,
+    mrn               VARCHAR(50)  NULL,
+    uan               VARCHAR(50)  NULL,
+    patient_uuid      VARCHAR(38),
     current_age       INT,
-    mobile_no         NVARCHAR(50),
-    date_of_birth     DATE          NULL,
-    sex               NVARCHAR(50)  NULL,
-    state_province    NVARCHAR(255) NULL,
-    county_district   NVARCHAR(255) NULL,
-    city_village      NVARCHAR(255) NULL,
-    coarse_age_group  NVARCHAR(255) NULL,
-    fine_age_group    NVARCHAR(255) NULL,
-    PRIMARY KEY (id)
+    mobile_no         VARCHAR(50),
+    phone_no         VARCHAR(50),
+    date_of_birth     DATE                               NULL,
+    sex               VARCHAR(50)  NULL,
+    state_province    VARCHAR(255) NULL,
+    county_district   VARCHAR(255) NULL,
+    city_village      VARCHAR(255) NULL,
+    coarse_age_group  VARCHAR(255) NULL,
+    fine_age_group    VARCHAR(255) NULL,
+    key_population    VARCHAR(50)   NULL,
+    marital_status    VARCHAR(50)   NULL,
+    education_level   VARCHAR(50)   NULL,
+    constraint client_id
+        unique (client_id)
 );
 CREATE INDEX mamba_dim_client_client_id_index ON mamba_dim_client (client_id);
 CREATE INDEX mamba_dim_client_mrn_index ON mamba_dim_client (mrn);
