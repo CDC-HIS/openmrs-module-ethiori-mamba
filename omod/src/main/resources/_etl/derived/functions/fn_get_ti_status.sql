daef0354fc0668cd7160b19d710b8969e4f2520f
samuelabebayehu <saa2280@cumc.columbia.edu>
2024-12-26 10:10:26 +0300

    fixed get ti status

--- omod/src/main/resources/_etl/derived/functions/fn_get_ti_status.sql
+++ omod/src/main/resources/_etl/derived/functions/fn_get_ti_status.sql
@@ -9,19 +9,20 @@ BEGIN
     DECLARE TI_RESULT NVARCHAR(10);
 
     -- Using EXISTS to check for a valid follow-up date within the specified range
-    IF EXISTS (SELECT 1
-               FROM mamba_flat_encounter_follow_up follow_up
-                        JOIN mamba_flat_encounter_follow_up_1 follow_up_1
-                             ON follow_up.encounter_id = follow_up_1.encounter_id
-                        JOIN mamba_flat_encounter_follow_up_2 follow_up_2
-                             ON follow_up.encounter_id = follow_up_2.encounter_id
-                        JOIN mamba_flat_encounter_follow_up_3 follow_up_3
-                             ON follow_up.encounter_id = follow_up_3.encounter_id
-               WHERE follow_up_status IS NOT NULL
-                 AND art_antiretroviral_start_date IS NOT NULL
-                 AND transferred_in_check_this_for_all_t IS NOT NULL
-                 AND follow_up.client_id = TI_CLIENT_ID
-                 AND follow_up_date_followup_ BETWEEN START_DATE AND END_DATE) THEN
+    IF EXISTS (select 1
+               from (SELECT MIN(follow_up_date_followup_) AS min_follow_up_date
+                     FROM mamba_flat_encounter_follow_up follow_up
+                              JOIN mamba_flat_encounter_follow_up_1 follow_up_1
+                                   ON follow_up.encounter_id = follow_up_1.encounter_id
+                              JOIN mamba_flat_encounter_follow_up_2 follow_up_2
+                                   ON follow_up.encounter_id = follow_up_2.encounter_id
+                              JOIN mamba_flat_encounter_follow_up_3 follow_up_3
+                                   ON follow_up.encounter_id = follow_up_3.encounter_id
+                     WHERE follow_up_status IS NOT NULL
+                       AND art_antiretroviral_start_date IS NOT NULL
+                       AND transferred_in_check_this_for_all_t IS NOT NULL
+                       AND follow_up.client_id = TI_CLIENT_ID) as a
+               where min_follow_up_date BETWEEN START_DATE AND END_DATE) THEN
         SET TI_RESULT = 'TI';
     ELSE
         SET TI_RESULT = 'NTI';
