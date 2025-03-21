1428dbf2756e6b7b7a0f79fd93d3d959842ef69a
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-03-21 15:18:25 +0300

    fixed applied for percentage and some qualifiers

--- omod/src/main/resources/_etl/derived/hmis_dhis_2/facts/hmis_dhis_2_queries/sp_fact_hmis_hiv_fp_query.sql
+++ omod/src/main/resources/_etl/derived/hmis_dhis_2/facts/hmis_dhis_2_queries/sp_fact_hmis_hiv_fp_query.sql
@@ -24,11 +24,11 @@ WITH FollowUp as (select follow_up.encounter_id,
                            left join mamba_flat_encounter_follow_up_4 follow_up_4
                                      on follow_up.encounter_id = follow_up_4.encounter_id),
      tx_curr_all AS (SELECT client_id,
-                            follow_up_date_followup_                                                                             AS FollowupDate,
+                            follow_up_date_followup_ AS FollowupDate,
                             encounter_id,
                             follow_up_status,
                             treatment_end_date,
-                            ROW_NUMBER() OVER (PARTITION BY client_id ORDER BY follow_up_date_followup_ DESC, encounter_id DESC) AS row_num
+                            ROW_NUMBER()                OVER (PARTITION BY client_id ORDER BY follow_up_date_followup_ DESC, encounter_id DESC) AS row_num
                      FROM FollowUp
                      WHERE follow_up_status IS NOT NULL
                        AND art_start_date IS NOT NULL
@@ -55,93 +55,97 @@ WITH FollowUp as (select follow_up.encounter_id,
                      join mamba_dim_client client on tmp_fp.client_id = client.client_id
                      join tx_curr on tmp_fp.client_id = tx_curr.client_id
             where tmp_fp.row_num = 1),
+     family_planning_wo_age as (select *
+                                from fp
+                                where sex = 'Female'
+                                  and (pregnancy_status = 'No' or pregnancy_status is null)),
      family_planning as (select *
                          from fp
-                         where sex = 'Female' and TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 15 AND 49
-                           and (pregnancy_status = 'No' or pregnancy_status is null)
-     )
+                         where sex = 'Female'
+                           and TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 15 AND 49
+                           and (pregnancy_status = 'No' or pregnancy_status is null))
 -- Number of non-pregnant women living with HIV on ART aged 15-49 reporting the use of any method of modern family planning by age
-SELECT 'HIV_ART_FP'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP'                                                                                                                      AS S_NO,
        'Number of non-pregnant women living with HIV on ART aged 15-49 reporting the use of any method of modern family planning by age' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 -- 10 - 14 years
 UNION ALL
-SELECT 'HIV_ART_FP. 1'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP. 1' AS S_NO,
        '10 - 14 years' as Activity,
-       COUNT(*)                                                                                                                as Value
-FROM family_planning
+       COUNT(*) as Value
+FROM family_planning_wo_age
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 10 AND 14
 -- 15 - 19 years
 UNION ALL
-SELECT 'HIV_ART_FP. 2'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP. 2' AS S_NO,
        '15 - 19 years' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 15 AND 19
 -- 20 - 24 years
 UNION ALL
-SELECT 'HIV_ART_FP. 3'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP. 3' AS S_NO,
        '20 - 24 years' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 20 AND 24
 -- 25 - 29 years
 UNION ALL
-SELECT 'HIV_ART_FP. 4'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP. 4' AS S_NO,
        '25 - 29 years' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 25 AND 29
 -- 30 - 49 years
 UNION ALL
-SELECT 'HIV_ART_FP. 5'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP. 5' AS S_NO,
        '30 - 49 years' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 WHERE TIMESTAMPDIFF(YEAR, date_of_birth, REPORT_END_DATE) BETWEEN 30 AND 49
 
 -- Number of non-pregnant women living with HIV on ART aged 15-49 reporting the use of any method of modern family planning by method
 UNION ALL
-SELECT 'HIV_ART_FP_MET'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP_MET'                                                                                                                     AS S_NO,
        'Number of non-pregnant women living with HIV on ART aged 15-49 reporting the use of any method of modern family planning by method' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 -- Oral contraceptives
 UNION ALL
-SELECT 'HIV_ART_FP_MET. 1'                                                                                                         AS S_NO,
+SELECT 'HIV_ART_FP_MET. 1'   AS S_NO,
        'Oral contraceptives' as Activity,
-       COUNT(*)                                                                                                                as Value
+       COUNT(*) as Value
 FROM family_planning
 WHERE method_of_family_planning = 'Oral contraception'
 -- Injectables
 UNION ALL
-SELECT 'HIV_ART_FP_MET. 2'                                                                                                         AS S_NO,
-       'Injectables' as Activity,
-       COUNT(*)                                                                                                                as Value
+SELECT 'HIV_ART_FP_MET. 2' AS S_NO,
+       'Injectables'       as Activity,
+       COUNT(*) as Value
 FROM family_planning
 WHERE method_of_family_planning='Injectable contraceptives'
 -- Implants
 UNION ALL
-SELECT 'HIV_ART_FP_MET. 3'                                                                                                         AS S_NO,
-       'Implants' as Activity,
-       COUNT(*)                                                                                                                as Value
+SELECT 'HIV_ART_FP_MET. 3' AS S_NO,
+       'Implants'          as Activity,
+       COUNT(*) as Value
 FROM family_planning
 WHERE method_of_family_planning='Implantable contraceptive (unspecified type)'
 -- IUCD
 UNION ALL
-SELECT 'HIV_ART_FP_MET. 4'                                                                                                         AS S_NO,
-       'IUCD' as Activity,
-       COUNT(*)                                                                                                                as Value
+SELECT 'HIV_ART_FP_MET. 4' AS S_NO,
+       'IUCD'              as Activity,
+       COUNT(*) as Value
 FROM family_planning
 WHERE method_of_family_planning=''
 -- Others
 UNION ALL
-SELECT 'HIV_ART_FP_MET. 5'                                                                                                         AS S_NO,
-       'Others' as Activity,
-       COUNT(*)                                                                                                                as Value
+SELECT 'HIV_ART_FP_MET. 5' AS S_NO,
+       'Others'            as Activity,
+       COUNT(*) as Value
 FROM family_planning
-WHERE method_of_family_planning='';
+WHERE method_of_family_planning not in ('Oral contraception','Injectable contraceptives','Implantable contraceptive (unspecified type)');
 END //
 
 DELIMITER ;
