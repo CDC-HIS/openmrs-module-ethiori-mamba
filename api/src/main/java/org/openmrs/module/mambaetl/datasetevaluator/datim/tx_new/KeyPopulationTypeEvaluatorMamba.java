9ce3eb9e543da745bdc96b6cd75232213a205b86
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-04-10 16:37:13 +0300

    tx new line list

--- api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/tx_new/KeyPopulationTypeEvaluatorMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/tx_new/KeyPopulationTypeEvaluatorMamba.java
@@ -1,7 +1,6 @@
 package org.openmrs.module.mambaetl.datasetevaluator.datim.tx_new;
 
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.BreastFeedingStatusDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.KeyPopulationTypeDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.ConnectionPoolManager;
 import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
