82744d7f18cf9cac24346416ae87469f187d8936
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-04-10 09:44:59 +0300

    datim tx new

---
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/tx_new/KeyPopulationTypeEvaluatorMamba.java
@@ -0,0 +1,53 @@
+package org.openmrs.module.mambaetl.datasetevaluator.datim.tx_new;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.BreastFeedingStatusDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.KeyPopulationTypeDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.helpers.ConnectionPoolManager;
+import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
+import org.openmrs.module.reporting.dataset.DataSet;
+import org.openmrs.module.reporting.dataset.SimpleDataSet;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.evaluator.DataSetEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Handler(supports = { KeyPopulationTypeDataSetDefinitionMamba.class })
+public class KeyPopulationTypeEvaluatorMamba implements DataSetEvaluator {
+	
+	@Override
+    public DataSet evaluate(DataSetDefinition dataSetDefinition, EvaluationContext evalContext) throws EvaluationException {
+        KeyPopulationTypeDataSetDefinitionMamba dataSetDefinitionMamba = new KeyPopulationTypeDataSetDefinitionMamba();
+        SimpleDataSet data = new SimpleDataSet(dataSetDefinition, evalContext);
+        ResultSetMapper resultSetMapper = new ResultSetMapper();
+        // Get ResultSet from the database
+        try (Connection connection = getDataSource().getConnection();
+             CallableStatement statement = connection.prepareCall("{call sp_dim_tx_new_datim_kp_query(?,?)}")) {
+
+            statement.setDate(1, new java.sql.Date(dataSetDefinitionMamba.getStartDate().getTime()));
+            statement.setDate(2, new java.sql.Date(dataSetDefinitionMamba.getEndDate().getTime()));
+
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet != null) {
+                    return resultSetMapper.mapResultSetToDataSet(resultSet, data);
+                } else {
+                    throw new EvaluationException("No data returned from the query.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new EvaluationException("Error processing ResultSet: " + e.getMessage(), e);
+        }
+
+    }
+	
+	private DataSource getDataSource() {
+		return ConnectionPoolManager.getInstance().getDataSource();
+	}
+}
--- api/src/main/java/org/openmrs/module/mambaetl/reports/datim/tx_new/TxNewDATIMReportsMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/datim/tx_new/TxNewDATIMReportsMamba.java
@@ -1,6 +1,8 @@
 package org.openmrs.module.mambaetl.reports.datim.tx_new;
 
+import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.BreastFeedingStatusDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.FineByAgeAndSexAndCD4DataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_new.KeyPopulationTypeDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.EthiOhriUtil;
 import org.openmrs.module.mambaetl.helpers.mapper.Cd4Status;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
@@ -16,100 +18,86 @@ import java.util.List;
 
 @Component
 public class TxNewDATIMReportsMamba implements ReportManager {
-	
-	@Override
-	public String getUuid() {
-		return "872b7ebd-ddc7-41dc-b9be-f3d44ec4ec74";
-	} //4d7b385f-331f-400c-8592-f539f4565d9d
-	
-	@Override
-	public String getName() {
-		return "MAMBA DATIM- TX_NEW";
-	}
-	
-	@Override
-	public String getDescription() {
-		return "TX new DATIM mamba report ";
-	}
-	
-	@Override
-	public List<Parameter> getParameters() {
-		return EthiOhriUtil.getDateRangeParameters();
-		
-	}
-	
-	@Override
-	public ReportDefinition constructReportDefinition() {
-		ReportDefinition reportDefinition = new ReportDefinition();
-		reportDefinition.setUuid(getUuid());
-		reportDefinition.setName(getName());
-		reportDefinition.setDescription(getDescription());
-		reportDefinition.setParameters(getParameters());
-		
-		// FINE BY AGE, SEX AND CD4 DATIM REPORTS
-		
-		FineByAgeAndSexAndCD4DataSetDefinitionMamba lessThan200CD4DataSetDefinitionMamba = new FineByAgeAndSexAndCD4DataSetDefinitionMamba();
-		lessThan200CD4DataSetDefinitionMamba.addParameters(getParameters());
-		lessThan200CD4DataSetDefinitionMamba.setCd4Status(Cd4Status.LOW);
-		lessThan200CD4DataSetDefinitionMamba.setDescription("Fine Age < 200 CD4");
-		reportDefinition.addDataSetDefinition("Fine Age < 200 CD4",
-		    EthiOhriUtil.map(lessThan200CD4DataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
-		FineByAgeAndSexAndCD4DataSetDefinitionMamba greaterThan200CD4DataSetDefinitionMamba = new FineByAgeAndSexAndCD4DataSetDefinitionMamba();
-		greaterThan200CD4DataSetDefinitionMamba.addParameters(getParameters());
-		greaterThan200CD4DataSetDefinitionMamba.setCd4Status(Cd4Status.HIGH);
-		greaterThan200CD4DataSetDefinitionMamba.setDescription("Fine Age >= 200 CD4");
-		reportDefinition.addDataSetDefinition("Fine Age > 200 CD4",
-		    EthiOhriUtil.map(greaterThan200CD4DataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
-		FineByAgeAndSexAndCD4DataSetDefinitionMamba unknownCD4DataSetDefinitionMamba = new FineByAgeAndSexAndCD4DataSetDefinitionMamba();
-		unknownCD4DataSetDefinitionMamba.addParameters(getParameters());
-		unknownCD4DataSetDefinitionMamba.setCd4Status(Cd4Status.UNKNOWN);
-		reportDefinition.addDataSetDefinition("Fine Age Unknown CD4",
-		    EthiOhriUtil.map(unknownCD4DataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
-//		// COARSE BY AGE, SEX AND CD4 DATIM REPORTS
-//
-//		CoarseByAgeAndSexAndCD4DataSetDefinitionMamba lessThan200CD4DataSetDefinitionMambaCoarse = new CoarseByAgeAndSexAndCD4DataSetDefinitionMamba();
-//		lessThan200CD4DataSetDefinitionMambaCoarse.addParameters(getParameters());
-//		lessThan200CD4DataSetDefinitionMambaCoarse.setCd4Status(Cd4Status.LOW);
-//		lessThan200CD4DataSetDefinitionMambaCoarse.setDescription("Coarse Age < 200 CD4");
-//		reportDefinition.addDataSetDefinition("Coarse Age < 200 CD4",
-//		    EthiOhriUtil.map(lessThan200CD4DataSetDefinitionMambaCoarse, "startDate=${startDateGC},endDate=${endDateGC}"));
-//
-//		CoarseByAgeAndSexAndCD4DataSetDefinitionMamba greaterThan200CD4DataSetDefinitionMambaCoarse = new CoarseByAgeAndSexAndCD4DataSetDefinitionMamba();
-//		greaterThan200CD4DataSetDefinitionMambaCoarse.addParameters(getParameters());
-//		greaterThan200CD4DataSetDefinitionMambaCoarse.setCd4Status(Cd4Status.HIGH);
-//		greaterThan200CD4DataSetDefinitionMambaCoarse.setDescription("Coarse Age >= 200 CD4");
-//		reportDefinition
-//		        .addDataSetDefinition("Coarse Age >= 200 CD4", EthiOhriUtil.map(
-//		            greaterThan200CD4DataSetDefinitionMambaCoarse, "startDate=${startDateGC},endDate=${endDateGC}"));
-//
-//		CoarseByAgeAndSexAndCD4DataSetDefinitionMamba unknownCD4DataSetDefinitionMambaCoarse = new CoarseByAgeAndSexAndCD4DataSetDefinitionMamba();
-//		unknownCD4DataSetDefinitionMambaCoarse.addParameters(getParameters());
-//		unknownCD4DataSetDefinitionMambaCoarse.setCd4Status(Cd4Status.UNKNOWN);
-//		reportDefinition.addDataSetDefinition("Coarse Age Unknown CD4",
-//		    EthiOhriUtil.map(unknownCD4DataSetDefinitionMambaCoarse, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
-		return reportDefinition;
-	}
-	
-	@Override
-	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-		
-		ReportDesign design = ReportManagerUtil.createExcelDesign("6d4cc920-e087-4b1c-9af2-287c80c0e0f8", reportDefinition);
-		
-		return Collections.singletonList(design);
-	}
-	
-	@Override
-	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
-		return null;
-	}
-	
-	@Override
-	public String getVersion() {
-		return "1.0.0-SNAPSHOT";
-	}
-	
+
+    @Override
+    public String getUuid() {
+        return "872b7ebd-ddc7-41dc-b9be-f3d44ec4ec74";
+    } //4d7b385f-331f-400c-8592-f539f4565d9d
+
+    @Override
+    public String getName() {
+        return "MAMBA DATIM- TX_NEW";
+    }
+
+    @Override
+    public String getDescription() {
+        return "TX new DATIM mamba report ";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        return EthiOhriUtil.getDateRangeParameters();
+
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+        ReportDefinition reportDefinition = new ReportDefinition();
+        reportDefinition.setUuid(getUuid());
+        reportDefinition.setName(getName());
+        reportDefinition.setDescription(getDescription());
+        reportDefinition.setParameters(getParameters());
+
+
+        FineByAgeAndSexAndCD4DataSetDefinitionMamba lessThan200CD4DataSetDefinitionMamba = new FineByAgeAndSexAndCD4DataSetDefinitionMamba();
+        lessThan200CD4DataSetDefinitionMamba.addParameters(getParameters());
+        lessThan200CD4DataSetDefinitionMamba.setCd4Status(Cd4Status.LOW);
+        lessThan200CD4DataSetDefinitionMamba.setDescription("<200 CD4");
+        reportDefinition.addDataSetDefinition("<200 CD4",
+                EthiOhriUtil.map(lessThan200CD4DataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+
+        FineByAgeAndSexAndCD4DataSetDefinitionMamba greaterThan200CD4DataSetDefinitionMamba = new FineByAgeAndSexAndCD4DataSetDefinitionMamba();
+        greaterThan200CD4DataSetDefinitionMamba.addParameters(getParameters());
+        greaterThan200CD4DataSetDefinitionMamba.setCd4Status(Cd4Status.HIGH);
+        greaterThan200CD4DataSetDefinitionMamba.setDescription(">= 200 CD4");
+        reportDefinition.addDataSetDefinition(">= 200 CD4",
+                EthiOhriUtil.map(greaterThan200CD4DataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+
+        FineByAgeAndSexAndCD4DataSetDefinitionMamba unknownCD4DataSetDefinitionMamba = new FineByAgeAndSexAndCD4DataSetDefinitionMamba();
+        unknownCD4DataSetDefinitionMamba.addParameters(getParameters());
+        unknownCD4DataSetDefinitionMamba.setCd4Status(Cd4Status.UNKNOWN);
+        reportDefinition.addDataSetDefinition("Unknown CD4",
+                EthiOhriUtil.map(unknownCD4DataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+
+        BreastFeedingStatusDataSetDefinitionMamba breastFeedingStatusDataSetDefinitionMamba = new BreastFeedingStatusDataSetDefinitionMamba();
+        breastFeedingStatusDataSetDefinitionMamba.addParameters(getParameters());
+        reportDefinition.addDataSetDefinition("Disaggregated by Breastfeeding Status at ART Initiation",
+                EthiOhriUtil.map(breastFeedingStatusDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+
+        KeyPopulationTypeDataSetDefinitionMamba keyPopulationTypeDataSetDefinitionMamba = new KeyPopulationTypeDataSetDefinitionMamba();
+        keyPopulationTypeDataSetDefinitionMamba.addParameters(getParameters());
+        reportDefinition.addDataSetDefinition("Disaggregated by key population type",
+                EthiOhriUtil.map(keyPopulationTypeDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+
+        return reportDefinition;
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+
+        ReportDesign design = ReportManagerUtil.createExcelDesign("6d4cc920-e087-4b1c-9af2-287c80c0e0f8", reportDefinition);
+
+        return Collections.singletonList(design);
+    }
+
+    @Override
+    public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0-SNAPSHOT";
+    }
+
 }
