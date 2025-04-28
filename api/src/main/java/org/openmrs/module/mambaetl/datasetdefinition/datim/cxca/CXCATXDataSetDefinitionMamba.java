cfc642a7deb3224025c62d07bfad7b6ad988da25
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-04-28 17:11:06 +0300

    cxca tx

---
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetdefinition/datim/cxca/CXCATXDataSetDefinitionMamba.java
@@ -0,0 +1,46 @@
+package org.openmrs.module.mambaetl.datasetdefinition.datim.cxca;
+
+import org.openmrs.module.mambaetl.helpers.reportOptions.CXCATXAggregationTypes;
+import org.openmrs.module.mambaetl.helpers.reportOptions.TxNewAggregationTypes;
+import org.openmrs.module.reporting.dataset.definition.BaseDataSetDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class CXCATXDataSetDefinitionMamba extends BaseDataSetDefinition {
+	
+	@ConfigurationProperty
+	private Date startDate;
+	
+	@ConfigurationProperty
+	private Date endDate;
+	
+	@ConfigurationProperty
+	private CXCATXAggregationTypes cxcatxAggregationTypes;
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	public CXCATXAggregationTypes getCxcatxAggregationTypes() {
+		return cxcatxAggregationTypes;
+	}
+	
+	public void setCxcatxAggregationTypes(CXCATXAggregationTypes cxcatxAggregationTypes) {
+		this.cxcatxAggregationTypes = cxcatxAggregationTypes;
+	}
+}
---
+++ api/src/main/java/org/openmrs/module/mambaetl/helpers/reportOptions/CXCATXAggregationTypes.java
@@ -0,0 +1,24 @@
+package org.openmrs.module.mambaetl.helpers.reportOptions;
+
+public enum CXCATXAggregationTypes {
+	FIRST_TIME_SCREENING("FIRST_TIME_SCREENING"), RE_SCREENING("RE_SCREENING"), POST_TREATMENT("POST_TREATMENT");
+	
+	private final String sqlValue;
+	
+	CXCATXAggregationTypes(String sqlValue) {
+		this.sqlValue = sqlValue;
+	}
+	
+	public String getSqlValue() {
+		return sqlValue;
+	}
+	
+	public static CXCATXAggregationTypes fromString(String status) {
+		for (CXCATXAggregationTypes cs : CXCATXAggregationTypes.values()) {
+			if (cs.name().equalsIgnoreCase(status) || cs.getSqlValue().equalsIgnoreCase(status)) {
+				return cs;
+			}
+		}
+		return FIRST_TIME_SCREENING;
+	}
+}
