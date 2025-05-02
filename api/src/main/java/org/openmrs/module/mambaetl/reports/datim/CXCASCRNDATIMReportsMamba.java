ee5ea233ee80333a35bff0fb4adc75383a6c32d3
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-05-02 14:33:40 +0300

    cxca scrn

--- api/src/main/java/org/openmrs/module/mambaetl/reports/datim/CXCASCRNDATIMReportsMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/datim/CXCASCRNDATIMReportsMamba.java
@@ -1,10 +1,10 @@
 package org.openmrs.module.mambaetl.reports.datim;
 
 import org.openmrs.module.mambaetl.datasetdefinition.datim.HeaderDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.datasetdefinition.datim.cxca.CXCASCRNDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.datasetdefinition.datim.cxca.CXCATXDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.EthiOhriUtil;
 import org.openmrs.module.mambaetl.helpers.reportOptions.CXCATXAggregationTypes;
-import org.openmrs.module.mambaetl.helpers.reportOptions.TxNewAggregationTypes;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.ReportRequest;
@@ -17,21 +17,21 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
-public class CXCATXDATIMReportsMamba implements ReportManager {
+public class CXCASCRNDATIMReportsMamba implements ReportManager {
 	
 	@Override
 	public String getUuid() {
-		return "f731add5-86b2-4af1-9a86-960262e1b06d";
+		return "be8377b4-cad5-4aea-91c8-8ee72f90cb7b";
 	} //4d7b385f-331f-400c-8592-f539f4565d9d
 	
 	@Override
 	public String getName() {
-		return "MAMBA DATIM- CXCA_TX";
+		return "MAMBA DATIM- CXCA_SCRN";
 	}
 	
 	@Override
 	public String getDescription() {
-		return "CXCA_TX DATIM mamba report ";
+		return "CXCA_SCRN DATIM mamba report ";
 	}
 	
 	@Override
@@ -49,30 +49,30 @@ public class CXCATXDATIMReportsMamba implements ReportManager {
 		reportDefinition.setParameters(getParameters());
 		
 		HeaderDataSetDefinitionMamba headerDefinition = new HeaderDataSetDefinitionMamba();
-		headerDefinition.setDescription("DSD: CXCA_TX");
+		headerDefinition.setDescription("DSD: CXCA_SCRN");
 		headerDefinition.setParameters(getParameters());
-		reportDefinition.addDataSetDefinition("DSD: CXCA_TX", EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
+		reportDefinition.addDataSetDefinition("DSD: CXCA_SCRN", EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
 		
-		CXCATXDataSetDefinitionMamba cxcatxFirstTimeDataSetDefinitionMamba = new CXCATXDataSetDefinitionMamba();
-		cxcatxFirstTimeDataSetDefinitionMamba.addParameters(getParameters());
-		cxcatxFirstTimeDataSetDefinitionMamba.setCxcatxAggregationTypes(CXCATXAggregationTypes.FIRST_TIME_SCREENING);
-		cxcatxFirstTimeDataSetDefinitionMamba.setDescription("First time screened for cervical cancer");
+		CXCASCRNDataSetDefinitionMamba cxcaScrnFirstTimeDataSetDefinitionMamba = new CXCASCRNDataSetDefinitionMamba();
+		cxcaScrnFirstTimeDataSetDefinitionMamba.addParameters(getParameters());
+		cxcaScrnFirstTimeDataSetDefinitionMamba.setCxcatxAggregationTypes(CXCATXAggregationTypes.FIRST_TIME_SCREENING);
+		cxcaScrnFirstTimeDataSetDefinitionMamba.setDescription("First time screened for cervical cancer");
 		reportDefinition.addDataSetDefinition("First time screened for cervical cancer",
-		    EthiOhriUtil.map(cxcatxFirstTimeDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+		    EthiOhriUtil.map(cxcaScrnFirstTimeDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
 		
-		CXCATXDataSetDefinitionMamba cxcatxRescreenedDataSetDefinitionMamba = new CXCATXDataSetDefinitionMamba();
-		cxcatxRescreenedDataSetDefinitionMamba.addParameters(getParameters());
-		cxcatxRescreenedDataSetDefinitionMamba.setCxcatxAggregationTypes(CXCATXAggregationTypes.RE_SCREENING);
-		cxcatxRescreenedDataSetDefinitionMamba.setDescription("Rescreened after previous negative or suspected cancer");
+		CXCASCRNDataSetDefinitionMamba cxcaScrnRescreenedDataSetDefinitionMamba = new CXCASCRNDataSetDefinitionMamba();
+		cxcaScrnRescreenedDataSetDefinitionMamba.addParameters(getParameters());
+		cxcaScrnRescreenedDataSetDefinitionMamba.setCxcatxAggregationTypes(CXCATXAggregationTypes.RE_SCREENING);
+		cxcaScrnRescreenedDataSetDefinitionMamba.setDescription("Rescreened after previous negative or suspected cancer");
 		reportDefinition.addDataSetDefinition("Rescreened after previous negative or suspected cancer",
-		    EthiOhriUtil.map(cxcatxRescreenedDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+		    EthiOhriUtil.map(cxcaScrnRescreenedDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
 		
-		CXCATXDataSetDefinitionMamba cxcatxPostTreatmentDataSetDefinitionMamba = new CXCATXDataSetDefinitionMamba();
-		cxcatxPostTreatmentDataSetDefinitionMamba.addParameters(getParameters());
-		cxcatxPostTreatmentDataSetDefinitionMamba.setCxcatxAggregationTypes(CXCATXAggregationTypes.POST_TREATMENT);
-		cxcatxPostTreatmentDataSetDefinitionMamba.setDescription("Post-treatment follow-up");
+		CXCASCRNDataSetDefinitionMamba cxcaScrnPostTreatmentDataSetDefinitionMamba = new CXCASCRNDataSetDefinitionMamba();
+		cxcaScrnPostTreatmentDataSetDefinitionMamba.addParameters(getParameters());
+		cxcaScrnPostTreatmentDataSetDefinitionMamba.setCxcatxAggregationTypes(CXCATXAggregationTypes.POST_TREATMENT);
+		cxcaScrnPostTreatmentDataSetDefinitionMamba.setDescription("Post-treatment follow-up");
 		reportDefinition.addDataSetDefinition("Post-treatment follow-up",
-		    EthiOhriUtil.map(cxcatxPostTreatmentDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+		    EthiOhriUtil.map(cxcaScrnPostTreatmentDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
 		
 		return reportDefinition;
 	}
@@ -80,7 +80,7 @@ public class CXCATXDATIMReportsMamba implements ReportManager {
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		
-		ReportDesign design = ReportManagerUtil.createExcelDesign("c272f59e-4aa4-451d-ad7e-97add8caac65", reportDefinition);
+		ReportDesign design = ReportManagerUtil.createExcelDesign("e4a79da6-6950-421a-bab3-20a547c06295", reportDefinition);
 		
 		return Collections.singletonList(design);
 	}
