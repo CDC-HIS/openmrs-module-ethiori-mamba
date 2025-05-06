6bb5766deccae2d8edd1b187aa30d163d69a4a28
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-05-06 11:40:27 +0300

    prep

--- omod/src/main/resources/_etl/derived/datim/sp_dim_prep_ct_datim_query.sql
+++ omod/src/main/resources/_etl/derived/datim/sp_dim_prep_ct_datim_query.sql
@@ -7,7 +7,6 @@ CREATE PROCEDURE sp_dim_prep_ct_datim_query(
     IN REPORT_END_DATE DATE,
     IN IS_COURSE_AGE_GROUP BOOLEAN,
     IN REPORT_TYPE VARCHAR(100)
-
 )
 BEGIN
 
@@ -32,7 +31,8 @@ BEGIN
                             follow_up.prep_dose_end_date,
                             follow_up.follow_up_date_followup_,
                             screening_date,
-                            hiv_test_result
+                            final_hiv_test_result,
+                            currently_breastfeeding_child
                      FROM mamba_flat_encounter_pre_exposure_scree screening
                               LEFT JOIN mamba_flat_encounter_pre_exposure_follo follow_up
                                         on screening.client_id = follow_up.client_id),
@@ -56,7 +56,9 @@ BEGIN
                                      client.client_id,
                                      follow_up_date_followup_,
                                      dose_dispensed,
-                                     hiv_test_result,
+                                     final_hiv_test_result,
+                                     f_pregnancy_status,
+                                     currently_breastfeeding_child,
                                      ROW_NUMBER() OVER (
                                          PARTITION BY PreExposure.client_id
                                          ORDER BY
@@ -85,7 +87,10 @@ BEGIN
                                    '`')
                )
         INTO age_group_cols
-        FROM (select normal_agegroup from mamba_dim_agegroup where mamba_dim_agegroup.datim_age_val >= 5 group by normal_agegroup) as order_query;
+        FROM (select normal_agegroup
+              from mamba_dim_agegroup
+              where mamba_dim_agegroup.datim_age_val >= 5
+              group by normal_agegroup) as order_query;
     ELSE
         SELECT GROUP_CONCAT(CONCAT('SUM(CASE WHEN fine_age_group = ''', datim_agegroup,
                                    ''' THEN count ELSE 0 END) AS `',
@@ -93,9 +98,12 @@ BEGIN
                                    '`')
                )
         INTO age_group_cols
-        FROM (select datim_agegroup from mamba_dim_agegroup where mamba_dim_agegroup.datim_age_val >= 5 group by datim_agegroup) as order_query;
+        FROM (select datim_agegroup
+              from mamba_dim_agegroup
+              where mamba_dim_agegroup.datim_age_val >= 5
+              group by datim_agegroup) as order_query;
     END IF;
-    IF REPORT_TYPE='AGE_SEX' THEN
+    IF REPORT_TYPE = 'AGE_SEX' THEN
         SET group_query = CONCAT('
         SELECT
           sex,
@@ -114,20 +122,32 @@ BEGIN
         USING (sex)
         GROUP BY sex
         ');
-    ELSEIF REPORT_TYPE='TEST_RESULT' THEN
-        SET group_query= 'select ''Positive'' as `Name`, COUNT(*) from prep where hiv_test_result=''Positive''
-UNION ALL
-select ''Negative'' as `Name`, COUNT(*) from prep where hiv_test_result=''Negative result''
-UNION ALL
-select ''Other'' as `Name`, COUNT(*) from prep where hiv_test_result NOT IN (''Negative result'',''Positive'')';
-    ELSEIF REPORT_TYPE='PREGNANT_BF' THEN
-    ELSEIF REPORT_TYPE='PREP_TYPE' THEN
+    ELSEIF REPORT_TYPE = 'TEST_RESULT' THEN
+        SET group_query = 'select ''Positive'' as `Name`, COUNT(*) as count from prep where final_hiv_test_result=''Positive''
+        UNION ALL
+        select ''Negative'' as `Name`, COUNT(*) from prep where final_hiv_test_result in (''Negative result'',''Negative'')
+        UNION ALL
+        select ''Other'' as `Name`, COUNT(*) from prep where final_hiv_test_result NOT IN (''Negative result'',''Positive'',''Negative'')';
+    ELSEIF REPORT_TYPE = 'PREP_TYPE' THEN
+        SET group_query = 'select ''Oral'' as `Name`, COUNT(*) as count from prep
+        UNION ALL
+        select ''Injectable'' as `Name`, 0
+        UNION ALL
+        select ''Other'' as `Name`, 0';
+    ELSEIF REPORT_TYPE = 'PREGNANT_BF' THEN
+        SET group_query = 'select ''Pregnant'' as `Name`, COUNT(*) as count from prep where f_pregnancy_status = ''Yes''
+        UNION ALL
+        select ''Breastfeeding'' as `Name`, COUNT(*) from prep where currently_breastfeeding_child = ''Yes''';
+    ELSEIF REPORT_TYPE = 'FACILITY' THEN
+        SET group_query = 'select ''Facility'' as `Name`, COUNT(*) as count from prep
+        UNION ALL
+        select ''Community (Associated with Facility)'' as `Name`, 0';
     END IF;
     SET @sql = CONCAT(prep_query, group_query);
     PREPARE stmt FROM @sql;
     SET @start_date = REPORT_START_DATE;
     SET @end_date = REPORT_END_DATE;
-    EXECUTE stmt USING @end_date , @start_date, @end_date , @start_date, @end_date , @start_date, @end_date;
+    EXECUTE stmt USING @start_date, @end_date , @start_date, @end_date ;
 
 END //
 
