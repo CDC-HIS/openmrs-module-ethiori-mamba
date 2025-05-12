dac29a16684a339bc2962a63868315c2a178b292
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-05-12 17:22:40 +0300

    analysis done

--- api/src/main/java/org/openmrs/module/mambaetl/datasetdefinition/linelist/TxCurrAnalysisLineListDataSetDefinitionMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetdefinition/linelist/TxCurrAnalysisLineListDataSetDefinitionMamba.java
@@ -13,15 +13,15 @@ public class TxCurrAnalysisLineListDataSetDefinitionMamba extends BaseDataSetDef
 	@ConfigurationProperty
 	private Date startDate;
 	
-	public Date getStartDate() {
-		return startDate;
-	}
-	
 	@ConfigurationProperty
 	private Date endDate;
 	
 	@ConfigurationProperty
 	private TxCurrAnalysisCategories txCurrAnalysisCategories;
+	
+	public Date getStartDate() {
+		return startDate;
+	}
 	
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
--- api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ProvidersViewLineListReportMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ProvidersViewLineListReportMamba.java
@@ -1,7 +1,5 @@
 package org.openmrs.module.mambaetl.reports.linelist;
 
-import java.util.*;
-
 import org.openmrs.module.mambaetl.datasetdefinition.linelist.TxCurrAnalysisLineListDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.EthiOhriUtil;
 import org.openmrs.module.mambaetl.helpers.reportOptions.TxCurrAnalysisCategories;
@@ -13,17 +11,19 @@ import org.openmrs.module.reporting.report.manager.ReportManager;
 import org.openmrs.module.reporting.report.manager.ReportManagerUtil;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+
 @Component
-public class TxCurrAnalysisLineListReportMamba implements ReportManager {
+public class ProvidersViewLineListReportMamba implements ReportManager {
 	
 	@Override
 	public String getUuid() {
-		return "73528f40-7987-482b-bc49-2d32451d00d9";
+		return "c0276e41-d668-41bc-a0e8-eeace2328376";
 	}
 	
 	@Override
 	public String getName() {
-		return "MAMBA LINELIST- TX_CURR_ANALYSIS";
+		return "MAMBA LINELIST- PROVERS VIEW";
 	}
 	
 	@Override
@@ -69,7 +69,7 @@ public class TxCurrAnalysisLineListReportMamba implements ReportManager {
 		TxCurrAnalysisLineListDataSetDefinitionMamba txCurrAnalysisDataSetDefinition = new TxCurrAnalysisLineListDataSetDefinitionMamba();
 		txCurrAnalysisDataSetDefinition.addParameters(getParameters());
 		
-		reportDefinition.addDataSetDefinition("List of Patients for TX Curr Analysis", EthiOhriUtil.map(
+		reportDefinition.addDataSetDefinition("List of Patients for Providers", EthiOhriUtil.map(
 		    txCurrAnalysisDataSetDefinition,
 		    "startDate=${startDateGC},endDate=${endDateGC},txCurrAnalysisCategories=${txCurrAnalysisCategories}"));
 		return reportDefinition;
@@ -78,7 +78,7 @@ public class TxCurrAnalysisLineListReportMamba implements ReportManager {
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		
-		ReportDesign design = ReportManagerUtil.createExcelDesign("ef7db8b4-aabd-4c57-a3f9-7cfb6aac7e3a", reportDefinition);
+		ReportDesign design = ReportManagerUtil.createExcelDesign("b9139849-9cf8-4d73-8644-31c47c3d0fce", reportDefinition);
 		
 		return Collections.singletonList(design);
 	}
