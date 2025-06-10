c5cf917a75333befe50dcc409810659efa3dd40c
samuel <saa2280@cumc.columbia.edu>
2025-06-10 12:40:17 +0300

    deployment

---
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_ahd_query.sql
@@ -0,0 +1,13 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_fact_line_list_ahd_query;
+
+CREATE PROCEDURE sp_fact_line_list_ahd_query(IN REPORT_END_DATE DATE)
+BEGIN
+
+
+
+END //
+
+DELIMITER ;
+unt or vl_status to be checked
