29cb05bc9f82a0102a83bdd04352bc81bbac0a51
samuel <saa2280@cumc.columbia.edu>
2025-06-12 16:33:00 +0300

    Build

---
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_re_test_query.sql
@@ -0,0 +1,12 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_fact_line_list_re_test_query;
+
+CREATE PROCEDURE sp_fact_line_list_re_test_query(IN REPORT_END_DATE DATE)
+BEGIN
+
+
+
+END //
+
+DELIMITER ;
