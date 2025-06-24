af84a9cabfb4dc5a2c2ecc23dc05017dbbb17dc2
samuel <saa2280@cumc.columbia.edu>
2025-06-24 16:17:34 +0300

    fix tx curr analysis options list

--- api/src/main/java/org/openmrs/module/mambaetl/helpers/reportOptions/TxCurrAnalysisCategories.java
+++ api/src/main/java/org/openmrs/module/mambaetl/helpers/reportOptions/TxCurrAnalysisCategories.java
@@ -1,8 +1,9 @@
 package org.openmrs.module.mambaetl.helpers.reportOptions;
 
 public enum TxCurrAnalysisCategories {
-	SUMMARY("Tx_Curr Summary"), TX_CURR_LAST_MONTH("Previous Month Tx_Curr"), TX_CURR_EXCLUDED_THIS_MONTH("Excluded From Tx_Curr"), NOT_UPDATED(
-			"Not Updated"), TX_CURR_NEWLY_INCLUDED("Newly Included Tx_Curr"),TX_CURR_THIS_MONTH("This Month TX_Curr"),  ON_DSD("Tx_Curr On DSD"), OTHER_OUTCOME("Other Outcome");
+	SUMMARY("Tx_Curr Summary"), TX_CURR_LAST_MONTH("Previous Month Tx_Curr"), TX_CURR_EXCLUDED_THIS_MONTH(
+	        "Excluded From Tx_Curr"), NOT_UPDATED("Not Updated"), TX_CURR_NEWLY_INCLUDED("Newly Included Tx_Curr"), TX_CURR_THIS_MONTH(
+	        "This Month TX_Curr"), ON_DSD("Tx_Curr On DSD"), OTHER_OUTCOME("Other Outcome");
 	
 	private final String sqlValue;
 	
