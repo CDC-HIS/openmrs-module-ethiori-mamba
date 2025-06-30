40ad0d51b986b2874847b789bcfa683c47aa8be3
samuel <saa2280@cumc.columbia.edu>
2025-06-30 08:20:51 +0300

    otz sp updated

--- api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/OTZLineListDatasetEvaluatorMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/OTZLineListDatasetEvaluatorMamba.java
@@ -72,7 +72,7 @@ public class OTZLineListDatasetEvaluatorMamba implements DataSetEvaluator {
         java.sql.Date endDate = dataSetDefinitionMamba.getEndDate() != null ? new java.sql.Date( dataSetDefinitionMamba.getEndDate().getTime()):null ;
 
         return Collections.singletonList(
-                new ProcedureCall("{call sp_fact_line_list_re_test_query(?,?)}", statement -> {
+                new ProcedureCall("{call sp_fact_line_list_otz_query(?,?)}", statement -> {
                         statement.setDate(1, startDate);
                         statement.setDate(2, endDate);
                 })
