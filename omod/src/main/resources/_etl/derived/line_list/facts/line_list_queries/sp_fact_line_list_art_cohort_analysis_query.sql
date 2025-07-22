0290c9aa800cc29dce60cd839f7e7324c3332d19
samuel <saa2280@cumc.columbia.edu>
2025-07-22 16:49:03 +0300

    cohort analysis done

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_art_cohort_analysis_query.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_art_cohort_analysis_query.sql
@@ -5,10 +5,6 @@ DROP PROCEDURE IF EXISTS sp_fact_line_list_art_cohort_analysis_query;
 CREATE PROCEDURE sp_fact_line_list_art_cohort_analysis_query(IN REPORT_START_DATE DATE, IN REPORT_END_DATE DATE)
 BEGIN
 
-    -- This procedure generates a line list for WHO ART Cohort Analysis.
-    -- It tracks outcomes and clinical details of patients at standard intervals after ART initiation.
-
-    -- CTE to get all follow-up encounters from the flat tables, including all required clinical details.
     WITH FollowUpEncounters AS (
         SELECT
             follow_up.encounter_id,
@@ -52,112 +48,110 @@ BEGIN
     ),
 
     -- Defines the cohort analysis intervals in months.
-    IntervalsDef AS (
-        SELECT 0 AS interval_month
-        UNION ALL
-        SELECT 6
-        UNION ALL
-        SELECT 12
-        UNION ALL
-        SELECT 24
-        UNION ALL
-        SELECT 36
-    ),
+         IntervalsDef AS (
+             SELECT 0 AS interval_month
+             UNION ALL
+             SELECT 6
+             UNION ALL
+             SELECT 12
+             UNION ALL
+             SELECT 24
+             UNION ALL
+             SELECT 36
+         ),
 
-    -- Creates an evaluation point for each patient at each interval.
-    PatientIntervals AS (
-        SELECT
-            a.PatientId,
-            a.art_start_date,
-            i.interval_month,
-            DATE_ADD(a.art_start_date, INTERVAL i.interval_month MONTH) as interval_end_date,
-            LAG(DATE_ADD(a.art_start_date, INTERVAL i.interval_month MONTH), 1, a.art_start_date) OVER (PARTITION BY a.PatientId ORDER BY i.interval_month) as interval_start_date
-        FROM ART_Initiation a
-        CROSS JOIN IntervalsDef i
-    WHERE DATE_ADD(a.art_start_date, INTERVAL i.interval_month MONTH) <= COALESCE(REPORT_END_DATE, CURDATE())
-    ),
+         -- Creates an evaluation point for each patient at each interval.
+         PatientIntervals AS (
+             SELECT
+                 a.PatientId,
+                 a.art_start_date,
+                 i.interval_month,
+                 DATE_ADD(a.art_start_date, INTERVAL i.interval_month MONTH) as interval_end_date,
+                 LAG(DATE_ADD(a.art_start_date, INTERVAL i.interval_month MONTH), 1, a.art_start_date) OVER (PARTITION BY a.PatientId ORDER BY i.interval_month) as interval_start_date
+             FROM ART_Initiation a
+                      CROSS JOIN IntervalsDef i
+         ),
 
-    -- Finds the latest follow-up for each patient within each interval to determine their status and general clinical details.
-    LatestFollowUpInInterval AS (
-        SELECT
-            pi.PatientId,
-            pi.interval_month,
-            pi.interval_end_date,
-            f.follow_up_date,
-            f.regimen,
-            f.ARTDoseDays,
-            f.follow_up_status,
-            f.AdherenceLevel,
-            f.pregnancy_status,
-            f.treatment_end_date,
-            f.next_visit_date,
-            f.cd4_count,
-            f.viral_load_count,
-            f.cd4_percent,
-            f.current_functional_status,
-            ROW_NUMBER() OVER(PARTITION BY pi.PatientId, pi.interval_month ORDER BY f.follow_up_date DESC, f.encounter_id DESC) as rn
-        FROM PatientIntervals pi
-        JOIN FollowUpEncounters f ON pi.PatientId = f.PatientId
-        WHERE f.follow_up_date >= pi.interval_start_date AND f.follow_up_date <= pi.interval_end_date
-    ),
+         -- Finds the latest follow-up for each patient within each interval to determine their status and general clinical details.
+         LatestFollowUpInInterval AS (
+             SELECT
+                 pi.PatientId,
+                 pi.interval_month,
+                 pi.interval_start_date,
+                 pi.interval_end_date,
+                 f.follow_up_date,
+                 f.regimen,
+                 f.ARTDoseDays,
+                 f.follow_up_status,
+                 f.AdherenceLevel,
+                 f.pregnancy_status,
+                 f.treatment_end_date,
+                 f.next_visit_date,
+                 f.cd4_count,
+                 f.viral_load_count,
+                 f.cd4_percent,
+                 f.current_functional_status,
+                 ROW_NUMBER() OVER(PARTITION BY pi.PatientId, pi.interval_month ORDER BY f.follow_up_date DESC, f.encounter_id DESC) as rn
+             FROM PatientIntervals pi
+                      LEFT JOIN  FollowUpEncounters f ON pi.PatientId = f.PatientId AND f.follow_up_date BETWEEN pi.interval_start_date AND pi.interval_end_date
 
-    -- Finds the latest Viral Load Sent record for each patient within each interval, independent of the main follow-up.
-    LatestViralLoadSentInInterval AS (
-        SELECT
-            pi.PatientId,
-            pi.interval_month,
-            f.viral_load_sent_date,
-            ROW_NUMBER() OVER(PARTITION BY pi.PatientId, pi.interval_month ORDER BY f.viral_load_sent_date DESC, f.encounter_id DESC) as rn_vl_sent
-        FROM PatientIntervals pi
-        JOIN FollowUpEncounters f ON pi.PatientId = f.PatientId
-        WHERE f.follow_up_date <= pi.interval_end_date and f.follow_up_date>=pi.art_start_date AND f.viral_load_sent_date IS NOT NULL
-    ),
-    -- Finds the latest Viral Load Performed record for each patient within each interval, independent of the main follow-up.
-    LatestViralLoadPerformedInInterval AS (
-        SELECT
-            pi.PatientId,
-            pi.interval_month,
-            f.viral_load_received_date,
-            f.viral_load_result,
-            ROW_NUMBER() OVER(PARTITION BY pi.PatientId, pi.interval_month ORDER BY f.viral_load_received_date DESC, f.encounter_id DESC) as rn_vl_performed
-        FROM PatientIntervals pi
-        JOIN FollowUpEncounters f ON pi.PatientId = f.PatientId
-        WHERE f.follow_up_date <= pi.interval_end_date and f.follow_up_date>=pi.art_start_date AND f.viral_load_received_date IS NOT NULL
-    ),
+         ),
+         -- Finds the latest Viral Load Sent record for each patient within each interval, independent of the main follow-up.
+         LatestViralLoadSentInInterval AS (
+             SELECT
+                 pi.PatientId,
+                 pi.interval_month,
+                 f.viral_load_sent_date,
+                 ROW_NUMBER() OVER(PARTITION BY pi.PatientId, pi.interval_month ORDER BY f.viral_load_sent_date DESC, f.encounter_id DESC) as rn_vl_sent
+             FROM PatientIntervals pi
+                      JOIN FollowUpEncounters f ON pi.PatientId = f.PatientId
+             WHERE f.follow_up_date <= pi.interval_end_date and f.follow_up_date>=pi.art_start_date AND f.viral_load_sent_date IS NOT NULL
+         ),
+         -- Finds the latest Viral Load Performed record for each patient within each interval, independent of the main follow-up.
+         LatestViralLoadPerformedInInterval AS (
+             SELECT
+                 pi.PatientId,
+                 pi.interval_month,
+                 f.viral_load_received_date,
+                 f.viral_load_result,
+                 ROW_NUMBER() OVER(PARTITION BY pi.PatientId, pi.interval_month ORDER BY f.viral_load_received_date DESC, f.encounter_id DESC) as rn_vl_performed
+             FROM PatientIntervals pi
+                      JOIN FollowUpEncounters f ON pi.PatientId = f.PatientId
+             WHERE f.follow_up_date <= pi.interval_end_date and f.follow_up_date>=pi.art_start_date AND f.viral_load_received_date IS NOT NULL
+         ),
 
-    -- Combines the latest follow-up details with the latest viral load details for each interval.
-    CohortDetails AS (
-        SELECT
-            lfu.PatientId,
-            lfu.interval_end_date,
-            lfu.interval_month,
-            CASE
-                WHEN lfu.follow_up_status IN ('Dead', 'Transferred out', 'Stop all', 'Loss to follow-up (LTFU)','Ran away') THEN lfu.follow_up_status
-                WHEN lfu.follow_up_status IN ('Alive', 'Restart medication') AND lfu.treatment_end_date >= lfu.interval_end_date THEN 'Active'
-                ELSE 'Lost to follow-up'
-            END AS outcome,
-            lfu.follow_up_date,
-            lfu.regimen,
-            lfu.viral_load_count,
-            lfu.ARTDoseDays,
-            lfu.follow_up_status,
-            lfu.AdherenceLevel,
-            lfu.pregnancy_status,
-            lfu.treatment_end_date,
-            lfu.next_visit_date,
-            viral_load_sent.viral_load_sent_date,
-            viral_load_performed.viral_load_received_date,
-            viral_load_performed.viral_load_result,
-            lfu.cd4_count,
-            lfu.cd4_percent,
-            lfu.current_functional_status
-        FROM (SELECT * FROM LatestFollowUpInInterval WHERE rn = 1) lfu
-        LEFT JOIN (SELECT * FROM LatestViralLoadSentInInterval WHERE rn_vl_sent = 1) viral_load_sent
-            ON lfu.PatientId = viral_load_sent.PatientId AND lfu.interval_month = viral_load_sent.interval_month
-        LEFT JOIN (SELECT * FROM LatestViralLoadPerformedInInterval WHERE rn_vl_performed = 1) viral_load_performed
-                  ON lfu.PatientId = viral_load_performed.PatientId AND lfu.interval_month = viral_load_performed.interval_month
-    )
-
+         -- Combines the latest follow-up details with the latest viral load details for each interval.
+         CohortDetails AS (
+             SELECT
+                 lfu.PatientId,
+                 lfu.interval_end_date,
+                 lfu.interval_month,
+                 CASE
+                     WHEN lfu.follow_up_status IN ('Dead', 'Transferred out', 'Stop all', 'Loss to follow-up (LTFU)','Ran away') THEN lfu.follow_up_status
+                     WHEN lfu.follow_up_status IN ('Alive', 'Restart medication') AND lfu.treatment_end_date >= lfu.interval_end_date THEN 'Active'
+                     ELSE 'Lost to follow-up'
+                     END AS outcome,
+                 lfu.follow_up_date,
+                 lfu.regimen,
+                 lfu.viral_load_count,
+                 lfu.ARTDoseDays,
+                 lfu.follow_up_status,
+                 lfu.AdherenceLevel,
+                 lfu.pregnancy_status,
+                 lfu.treatment_end_date,
+                 lfu.next_visit_date,
+                 viral_load_sent.viral_load_sent_date,
+                 viral_load_performed.viral_load_received_date,
+                 viral_load_performed.viral_load_result,
+                 lfu.cd4_count,
+                 lfu.cd4_percent,
+                 lfu.current_functional_status
+             FROM (SELECT * FROM LatestFollowUpInInterval WHERE rn = 1) lfu
+                      LEFT JOIN (SELECT * FROM LatestViralLoadSentInInterval WHERE rn_vl_sent = 1) viral_load_sent
+                                ON lfu.PatientId = viral_load_sent.PatientId AND lfu.interval_month = viral_load_sent.interval_month
+                      LEFT JOIN (SELECT * FROM LatestViralLoadPerformedInInterval WHERE rn_vl_performed = 1) viral_load_performed
+                                ON lfu.PatientId = viral_load_performed.PatientId AND lfu.interval_month = viral_load_performed.interval_month
+         )
     -- Final SELECT statement to generate the line list, pivoting all details into columns for each interval.
     SELECT
         dc.patient_name AS 'Patient Name',
