1d6109f3daed83d6c97af3abf03a0805ef496e81
abdisha <asresabdi@gmail.com>
2025-08-12 12:14:20 +0300

    done with Schedule visit

---
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetdefinition/linelist/ScheduleVisitLineListDataSetDefinitionMamba.java
@@ -0,0 +1,34 @@
+package org.openmrs.module.mambaetl.datasetdefinition.linelist;
+
+import org.openmrs.module.reporting.dataset.definition.BaseDataSetDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class ScheduleVisitLineListDataSetDefinitionMamba extends BaseDataSetDefinition {
+	
+	@ConfigurationProperty
+	private Date startDate;
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	@ConfigurationProperty
+	private Date endDate;
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+}
