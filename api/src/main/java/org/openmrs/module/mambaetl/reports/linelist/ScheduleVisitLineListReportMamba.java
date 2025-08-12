4226f764a57795b7975e303032ecc45e5c673758
abdisha <asresabdi@gmail.com>
2025-08-12 12:14:20 +0300

    done with Schedule visit

---
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ScheduleVisitLineListReportMamba.java
@@ -0,0 +1,76 @@
+package org.openmrs.module.mambaetl.reports.linelist;
+
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.MonthlyVisitDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.ScheduleVisitLineListDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.helpers.EthiOhriUtil;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.ReportRequest;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.manager.ReportManager;
+import org.openmrs.module.reporting.report.manager.ReportManagerUtil;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.openmrs.module.mambaetl.helpers.EthiOhriUtil.map;
+
+@Component
+public class ScheduleVisitLineListReportMamba implements ReportManager {
+	
+	@Override
+	public String getUuid() {
+		return "691327fb-4596-7622-xxx-27ee765y43ef";
+	}
+	
+	@Override
+	public String getName() {
+		return "LINELIST- Schedule Visit-Mamba";
+	}
+	
+	@Override
+	public String getDescription() {
+		return null;
+	}
+	
+	@Override
+	public List<Parameter> getParameters() {
+		return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
+	}
+	
+	@Override
+	public ReportDefinition constructReportDefinition() {
+		ReportDefinition reportDefinition = new ReportDefinition();
+		reportDefinition.setUuid(getUuid());
+		reportDefinition.setName(getName());
+		reportDefinition.setDescription(getDescription());
+		
+		reportDefinition.setParameters(getParameters());
+		
+		ScheduleVisitLineListDataSetDefinitionMamba dataSetDefinitionMamba = new ScheduleVisitLineListDataSetDefinitionMamba();
+		dataSetDefinitionMamba.addParameters(getParameters());
+		
+		reportDefinition.addDataSetDefinition("Schedule Visit mamaba line list",
+		    map(dataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+		return reportDefinition;
+	}
+	
+	@Override
+	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+		ReportDesign design = ReportManagerUtil.createExcelDesign("12345678-0000-4910-a1dc-8149b7696817", reportDefinition);
+		
+		return Collections.singletonList(design);
+	}
+	
+	@Override
+	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
+		return new ArrayList<>();
+	}
+	
+	@Override
+	public String getVersion() {
+		return "1.0.0-SNAPSHOT";
+	}
+}
