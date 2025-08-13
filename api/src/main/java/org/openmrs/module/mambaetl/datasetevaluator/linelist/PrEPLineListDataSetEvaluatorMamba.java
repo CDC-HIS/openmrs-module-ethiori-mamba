010be2e4daab256eee31e21741bd77686b21b4df
abdisha <asresabdi@gmail.com>
2025-08-13 14:05:39 +0300

    done with prEp line list

---
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/PrEPLineListDataSetEvaluatorMamba.java
@@ -0,0 +1,79 @@
+package org.openmrs.module.mambaetl.datasetevaluator.linelist;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.AHDLineListDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.PrEPLineListDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper;
+import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
+import org.openmrs.module.reporting.dataset.DataSet;
+import org.openmrs.module.reporting.dataset.SimpleDataSet;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.evaluator.DataSetEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import static org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper.*;
+
+@Handler(supports = { PrEPLineListDataSetDefinitionMamba.class })
+public class PrEPLineListDataSetEvaluatorMamba implements DataSetEvaluator {
+	
+	private static final Log log = LogFactory.getLog(PrEPLineListDataSetEvaluatorMamba.class);
+	
+	private static final String ERROR_PROCESSING_RESULT_SET = "Error processing ResultSet: ";
+	
+	private static final String DATABASE_CONNECTION_ERROR = "Database connection error: ";
+	
+	@Override
+	public DataSet evaluate(DataSetDefinition dataSetDefinition, EvaluationContext evalContext)
+			throws EvaluationException {
+
+		PrEPLineListDataSetDefinitionMamba definition = (PrEPLineListDataSetDefinitionMamba) dataSetDefinition;
+		SimpleDataSet data = new SimpleDataSet(dataSetDefinition, evalContext);
+		ResultSetMapper resultSetMapper = new ResultSetMapper();
+
+		try (Connection connection = DataSetEvaluatorHelper.getDataSource().getConnection()) {
+			connection.setAutoCommit(false);
+
+			List<ProcedureCall> procedureCalls = createProcedureCalls(definition);
+
+			try (CallableStatementContainer statementContainer = prepareStatements(connection, procedureCalls)) {
+
+				executeStatements(statementContainer, procedureCalls);
+
+				ResultSet[] allResultSets = statementContainer.getResultSets();
+				mapResultSet(data, resultSetMapper, allResultSets,Boolean.TRUE);
+				connection.commit();
+				return data;
+
+			} catch (SQLException e) {
+				rollbackAndThrowException(connection, ERROR_PROCESSING_RESULT_SET + e.getMessage(), e, log);
+			}
+		} catch (SQLException e) {
+			throw new EvaluationException(DATABASE_CONNECTION_ERROR + e.getMessage(), e);
+		}
+		return null;
+	}
+	
+	private List<ProcedureCall> createProcedureCalls(PrEPLineListDataSetDefinitionMamba dataSetDefinitionMamba) {
+		java.sql.Date startDate = dataSetDefinitionMamba.getStartDate() != null ?
+				new java.sql.Date( dataSetDefinitionMamba.getStartDate().getTime()): new java.sql.Date(LocalDate.of(1900, 1, 1).toEpochDay() * 24 * 60 * 60 * 1000);
+		java.sql.Date endDate = dataSetDefinitionMamba.getEndDate() != null ?
+				new java.sql.Date( dataSetDefinitionMamba.getEndDate().getTime()): new java.sql.Date(System.currentTimeMillis());
+
+		return Collections.singletonList(
+                new ProcedureCall("{call sp_fact_line_list_pre_exposure_query(?,?)}", statement -> {
+					statement.setDate(1, startDate);
+					statement.setDate(2, endDate);
+                })
+        );
+	}
+}
