b7f5203b38f619d07e641a55a04f34737725da9c
samuel <saa2280@cumc.columbia.edu>
2025-08-14 17:03:54 +0300

    ICT commit

---
+++ omod/src/main/resources/_etl/derived/datim/sp_dim_ict_datim_query.sql
@@ -0,0 +1,15 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_dim_ict_datim_query;
+
+CREATE PROCEDURE sp_dim_ict_datim_query(
+    IN REPORT_END_DATE DATE
+)
+
+WITH FollowUp as (  )
+BEGIN
+
+
+END //
+
+DELIMITER ;
