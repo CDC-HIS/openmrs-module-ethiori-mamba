b9d17d4392c412a016d65524ddc6a8c6689525d0
samuel <saa2280@cumc.columbia.edu>
2025-09-02 10:50:48 +0300

    Update mamba core version

--- api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ICTContactLineListReport.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ICTContactLineListReport.java
@@ -15,59 +15,60 @@ import java.util.List;
 
 @Component
 public class ICTContactLineListReport implements ReportManager {
-    @Override
-    public String getUuid() {
-        return "9001d52e-1dec-4e34-b1f4-1d22612597f8";
-    }
-
-    @Override
-    public String getName() {
-        return "LINELIST- ICT_CONTACT";
-    }
-
-    @Override
-    public String getDescription() {
-        return "ICT CONTACT";
-    }
-
-    @Override
-    public List<Parameter> getParameters() {
-        return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
-
-    }
-
-    @Override
-    public ReportDefinition constructReportDefinition() {
-        ReportDefinition reportDefinition = new ReportDefinition();
-        reportDefinition.setUuid(getUuid());
-        reportDefinition.setName(getName());
-        reportDefinition.setDescription(getDescription());
-        reportDefinition.setParameters(getParameters());
-
-        ICTContactLineListDataSetDefinitionMamba ictContactLineListDataSetDefinitionMamba = new ICTContactLineListDataSetDefinitionMamba();
-        ictContactLineListDataSetDefinitionMamba.addParameters(getParameters());
-        reportDefinition.addDataSetDefinition("List of ICT Contacts",
-                EthiOhriUtil.map(ictContactLineListDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
-
-        return reportDefinition;
-    }
-
-    @Override
-    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-
-        ReportDesign design = ReportManagerUtil.createExcelDesign("cd346276-2740-44ca-8195-021928d588ee", reportDefinition);
-        design.setReportDefinition(reportDefinition);
-
-        return Collections.singletonList(design);
-    }
-
-    @Override
-    public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
-        return null;
-    }
-
-    @Override
-    public String getVersion() {
-        return "1.0.0-SNAPSHOT";
-    }
+	
+	@Override
+	public String getUuid() {
+		return "9001d52e-1dec-4e34-b1f4-1d22612597f8";
+	}
+	
+	@Override
+	public String getName() {
+		return "LINELIST- ICT_CONTACT";
+	}
+	
+	@Override
+	public String getDescription() {
+		return "ICT CONTACT";
+	}
+	
+	@Override
+	public List<Parameter> getParameters() {
+		return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
+		
+	}
+	
+	@Override
+	public ReportDefinition constructReportDefinition() {
+		ReportDefinition reportDefinition = new ReportDefinition();
+		reportDefinition.setUuid(getUuid());
+		reportDefinition.setName(getName());
+		reportDefinition.setDescription(getDescription());
+		reportDefinition.setParameters(getParameters());
+		
+		ICTContactLineListDataSetDefinitionMamba ictContactLineListDataSetDefinitionMamba = new ICTContactLineListDataSetDefinitionMamba();
+		ictContactLineListDataSetDefinitionMamba.addParameters(getParameters());
+		reportDefinition.addDataSetDefinition("List of ICT Contacts",
+		    EthiOhriUtil.map(ictContactLineListDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+		
+		return reportDefinition;
+	}
+	
+	@Override
+	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+		
+		ReportDesign design = ReportManagerUtil.createExcelDesign("cd346276-2740-44ca-8195-021928d588ee", reportDefinition);
+		design.setReportDefinition(reportDefinition);
+		
+		return Collections.singletonList(design);
+	}
+	
+	@Override
+	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
+		return null;
+	}
+	
+	@Override
+	public String getVersion() {
+		return "1.0.0-SNAPSHOT";
+	}
 }
--- api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ICTScreeningLineListReport.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/ICTScreeningLineListReport.java
@@ -16,59 +16,60 @@ import java.util.List;
 
 @Component
 public class ICTScreeningLineListReport implements ReportManager {
-    @Override
-    public String getUuid() {
-        return "7ad2b648-ea3a-42af-bc24-dbb265b0fc87";
-    }
-
-    @Override
-    public String getName() {
-        return "LINELIST- ICT_SCREENING";
-    }
-
-    @Override
-    public String getDescription() {
-        return "ICT SCREENING";
-    }
-
-    @Override
-    public List<Parameter> getParameters() {
-        return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
-
-    }
-
-    @Override
-    public ReportDefinition constructReportDefinition() {
-        ReportDefinition reportDefinition = new ReportDefinition();
-        reportDefinition.setUuid(getUuid());
-        reportDefinition.setName(getName());
-        reportDefinition.setDescription(getDescription());
-        reportDefinition.setParameters(getParameters());
-
-        ICTScreeningLineListDataSetDefinitionMamba ictScreeningLineListDataSetDefinitionMamba = new ICTScreeningLineListDataSetDefinitionMamba();
-        ictScreeningLineListDataSetDefinitionMamba.addParameters(getParameters());
-        reportDefinition.addDataSetDefinition("List of ICT Screening",
-                EthiOhriUtil.map(ictScreeningLineListDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
-
-        return reportDefinition;
-    }
-
-    @Override
-    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-
-        ReportDesign design = ReportManagerUtil.createExcelDesign("946b6f19-4f2a-4213-a205-e64fcd260827", reportDefinition);
-        design.setReportDefinition(reportDefinition);
-
-        return Collections.singletonList(design);
-    }
-
-    @Override
-    public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
-        return null;
-    }
-
-    @Override
-    public String getVersion() {
-        return "1.0.0-SNAPSHOT";
-    }
+	
+	@Override
+	public String getUuid() {
+		return "7ad2b648-ea3a-42af-bc24-dbb265b0fc87";
+	}
+	
+	@Override
+	public String getName() {
+		return "LINELIST- ICT_SCREENING";
+	}
+	
+	@Override
+	public String getDescription() {
+		return "ICT SCREENING";
+	}
+	
+	@Override
+	public List<Parameter> getParameters() {
+		return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
+		
+	}
+	
+	@Override
+	public ReportDefinition constructReportDefinition() {
+		ReportDefinition reportDefinition = new ReportDefinition();
+		reportDefinition.setUuid(getUuid());
+		reportDefinition.setName(getName());
+		reportDefinition.setDescription(getDescription());
+		reportDefinition.setParameters(getParameters());
+		
+		ICTScreeningLineListDataSetDefinitionMamba ictScreeningLineListDataSetDefinitionMamba = new ICTScreeningLineListDataSetDefinitionMamba();
+		ictScreeningLineListDataSetDefinitionMamba.addParameters(getParameters());
+		reportDefinition.addDataSetDefinition("List of ICT Screening",
+		    EthiOhriUtil.map(ictScreeningLineListDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
+		
+		return reportDefinition;
+	}
+	
+	@Override
+	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+		
+		ReportDesign design = ReportManagerUtil.createExcelDesign("946b6f19-4f2a-4213-a205-e64fcd260827", reportDefinition);
+		design.setReportDefinition(reportDefinition);
+		
+		return Collections.singletonList(design);
+	}
+	
+	@Override
+	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
+		return null;
+	}
+	
+	@Override
+	public String getVersion() {
+		return "1.0.0-SNAPSHOT";
+	}
 }
