1095dcd6d88dcc1238beb9f17f926f46aacd06c0
G/yohannes Zenebe <gyohanneszen@gmail.com>
2025-09-17 10:42:53 +0300

    fix ncd report query

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_ncd_screening_and_treatment_query.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_ncd_screening_and_treatment_query.sql
@@ -1,119 +1,205 @@
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS sp_fact_line_list_phrh_sns_query;
+DROP PROCEDURE IF EXISTS sp_fact_line_list_ncd_screening_and_treatment_query;
 
-CREATE PROCEDURE sp_fact_line_list_phrh_sns_query(IN REPORT_START_DATE DATE, IN REPORT_END_DATE DATE, IN PHRH_CODE VARCHAR(255))
+CREATE PROCEDURE sp_fact_line_list_ncd_screening_and_treatment_query(IN REPORT_START_DATE DATE, IN REPORT_END_DATE DATE)
 BEGIN
-	WITH provided_coupons AS (
+
+	WITH screening AS (
         SELECT
-            mps.client_id,
-            mps.followup_date,
-            mps.coupon_1  AS coupon_id,
-            mpf.final_hiv_test_result AS conventional_result
-        FROM mamba_flat_encounter_phrh_sns mps
-        INNER JOIN mamba_flat_encounter_phrh_followup mpf
-			ON mps.client_id = mpf.client_id
-		WHERE mps.coupon_1 is not null
-
-        UNION
-
-        SELECT
-            mps.client_id,
-            mps.followup_date,
-            mps.coupon_2  AS coupon_id,
-            mpf.final_hiv_test_result AS conventional_result
-        FROM mamba_flat_encounter_phrh_sns mps
-        INNER JOIN mamba_flat_encounter_phrh_followup mpf
-			ON mps.client_id = mpf.client_id
-		WHERE mps.coupon_2 != ''
-
-        UNION
-
-        SELECT
-            mps.client_id,
-            mps.followup_date,
-            mps.coupon_3  AS coupon_id,
-            mpf.final_hiv_test_result AS conventional_result
-        FROM mamba_flat_encounter_phrh_sns mps
-        INNER JOIN mamba_flat_encounter_phrh_followup mpf
-			ON mps.client_id = mpf.client_id
-		WHERE mps.coupon_3 != ''
-
-        UNION
-
-        SELECT
-            mps.client_id,
-            mps.followup_date,
-            mps.coupon_4  AS coupon_id,
-            mpf.final_hiv_test_result AS conventional_result
-        FROM mamba_flat_encounter_phrh_sns mps
-        INNER JOIN mamba_flat_encounter_phrh_followup mpf
-			ON mps.client_id = mpf.client_id
-		WHERE mps.coupon_4 != ''
-
-        UNION
-
-        SELECT
-            mps.client_id,
-            mps.followup_date,
-            mps.coupon_5  AS coupon_id,
-            mpf.final_hiv_test_result AS conventional_result
-        FROM mamba_flat_encounter_phrh_sns mps
-        INNER JOIN mamba_flat_encounter_phrh_followup mpf
-			ON mps.client_id = mpf.client_id
-		WHERE mps.coupon_5 != ''
+			mpn.client_id,
+			mpc.mrn,
+			mpc.uan,
+			mpc.patient_name AS full_name,
+			mpc.sex AS gender,
+			TIMESTAMPDIFF(YEAR, mpc.date_of_birth, CURDATE()) AS age,
+			mpc.state_province AS region,
+			mpc.county_district AS zone,
+			mpc.city_village AS woreda,
+			mpc.mobile_no AS phone_number,
+            mpn.screening_date
+		FROM mamba_flat_encounter_ncd_screening mpn
+        INNER JOIN mamba_dim_client mpc
+			ON mpn.client_id = mpc.client_id
     ),
-    provided_date AS (
-		SELECT p.*
-			FROM provided_coupons p
-            WHERE REPORT_START_DATE is not null and REPORT_END_DATE is not null and (p.followup_date BETWEEN REPORT_START_DATE and REPORT_END_DATE) or
-			(REPORT_START_DATE is not null and REPORT_END_DATE is null and (p.followup_date >= REPORT_START_DATE)) or
-			(REPORT_START_DATE is null and REPORT_END_DATE is not null and (p.followup_date <= REPORT_END_DATE)) or
+	ncd_followup AS (
+		SELECT mpn.*,
+			ROW_NUMBER() OVER (PARTITION BY mpn.client_id ORDER BY mpnf.follow_up_date DESC, mpnf.encounter_id DESC) AS ncd_followup_row_num,
+			ROW_NUMBER() OVER (PARTITION BY mpn.client_id ORDER BY mpf6.follow_up_date_followup_ DESC, mpf6.encounter_id DESC) AS art_followup_row_num,
+            fn_gregorian_to_ethiopian_calendar(mpf2.date_of_event, 'Y-M-D') AS hiv_confirmed_date_ec,
+			mpf2.date_of_event AS hiv_confirmed_date,
+			fn_gregorian_to_ethiopian_calendar(mpf7.art_antiretroviral_start_date, 'Y-M-D') AS art_start_date_ec,
+			mpf7.art_antiretroviral_start_date AS art_start_date,
+			mpf6.follow_up_date_followup_ AS art_followup_date,
+			COALESCE(mpnf1.diabetes_mellitus_diagnose_type, mpnf1.hypertension_diagnose_type) AS diagnosis_type,
+            mpnf1.baseline_diagnosis AS diagnosis,
+			mpnf1.ncd_clinical_status AS outcome,
+			fn_gregorian_to_ethiopian_calendar(mpnf1.return_visit_date, 'Y-M-D') AS next_appointment_date_ec,
+			mpnf1.return_visit_date AS next_appointment_date,
+			fn_gregorian_to_ethiopian_calendar(mpnf.follow_up_date, 'Y-M-D') AS ncd_follow_up_date_ec,
+			mpnf.follow_up_date AS ncd_follow_up_date,
+			mpf4.follow_up_status AS art_followup_status,
+			mpf1.regimen AS art_regimen,
+			mpf8.antiretroviral_art_dispensed_dose_i AS art_regimen_dose,
+			mpnf.visit_type,
+			CONCAT_WS(',',
+				mpnf.use_of_tobacco,
+				mpnf.excessive_alcohol,
+				mpnf1.physical_inactivity,
+				mpnf1.high_salt_consumption,
+				mpnf1.high_sugar_consumption,
+				mpnf1.high_fat_consumption,
+				mpnf1.low_vegetable_fruit_consumption,
+				mpnf1.family_history_of_dm_htn_cvd,
+				mpnf.khat_consumption
+			) AS risk_factors,
+			CONCAT_WS(',',
+				mpnf.polyuria,
+				mpnf.nocturia,
+				mpnf.polydipsia,
+				mpnf.fatigue,
+				mpnf.nausea,
+				mpnf.vomiting,
+				mpnf.deep_breathing,
+				mpnf.fast_breathing,
+				mpnf.weakness,
+				mpnf.abdominal_pain,
+				mpnf.hunger,
+				mpnf.palpitations,
+				mpnf.lightheadedness,
+				mpnf.dizziness,
+				mpnf.sweating,
+				mpnf.extreme_fatigue,
+				mpnf.confusion,
+				mpnf.bad_dreams,
+				mpnf.fever,
+				mpnf.cough,
+				mpnf.skin_lesions,
+				mpnf.dysuria,
+				mpnf1.frequent_urination,
+				mpnf1.diabetic_foot_ulcer,
+				mpnf.tingling,
+				mpnf.numbness,
+				mpnf.burning_sensation,
+				mpnf1.postural_dizziness,
+				mpnf1.sexual_dysfunction,
+				mpnf.visual_blurring
+			) AS dm_symptoms,
+			CONCAT_WS(',',
+				mpnf.dyspnea,
+				mpnf.orthopnea,
+				mpnf1.paroxysmal_nocturnal_dyspnea,
+				mpnf.leg_swelling,
+				mpnf.angina,
+				mpnf1.intermittent_claudication,
+				mpnf1.hemiparesis_hemiplegia,
+				mpnf1.speech_abnormality,
+				mpnf.seizure,
+				mpnf.headache,
+				mpnf.photophobia,
+				mpnf.neck_pain
+			) AS hypertension_symptoms,
+			mpnf1.systolic_blood_pressure AS bp1,
+			mpnf1.diastolic_blood_pressure AS bp2,
+            mpnf.bmi_text_ AS bmi,
+			mpnf1.fasting_blood_glucose_measurement_m AS fbs,
+			mpnf1.fundoscopic_exam_finding AS fundoscopy,
+			CONCAT_WS(',',
+				mpnf1.chronic_kidney_disease,
+				mpnf1.transient_ischemic_attack,
+				mpnf1.hypertensive_heart_disease,
+				mpnf1.peripheral_arterial_disease,
+				mpnf1.diabetic_retinopathy,
+				mpnf1.diabetic_neuropathy,
+				mpnf1.diabetic_nephropathy,
+				mpnf1.coronary_artery_disease,
+				mpnf1.diabetic_foot_ulcer,
+				mpnf1.diabetes_ketoacidosis
+			) AS dm_complications,
+			CONCAT_WS(',',
+				mpnf1.chronic_kidney_disease,
+				mpnf1.transient_ischemic_attack,
+				mpnf1.hypertensive_heart_disease,
+				mpnf1.peripheral_arterial_disease,
+				mpnf1.coronary_artery_disease
+			) AS htn_complications,
+			mpnf1.overall_treatment_adherence AS adherence,
+			CONCAT_WS(',',
+				mpnf1.cessation_of_smoking,
+				mpnf1.exercise_150min_week,
+				mpnf.weight_reduction,
+				mpnf.healthy_diet,
+				mpnf1.medication_adherence_side_effect,
+				mpnf1.daily_foot_inspection,
+				mpnf.family_screening,
+				mpnf1.injection_techniques
+			) AS lifestyle_changes
+	FROM screening mpn
+		LEFT JOIN mamba_flat_encounter_ncd_followup mpnf
+			ON mpn.client_id = mpnf.client_id
+		LEFT JOIN mamba_flat_encounter_ncd_followup_1 mpnf1
+			ON mpnf.encounter_id = mpnf1.encounter_id
+		LEFT JOIN mamba_flat_encounter_follow_up mpf
+			ON mpn.client_id = mpf.client_id
+        LEFT JOIN mamba_flat_encounter_follow_up_1 mpf1
+			ON mpf.encounter_id = mpf1.encounter_id
+		LEFT JOIN mamba_flat_encounter_follow_up_2 mpf2
+			ON mpf.encounter_id = mpf2.encounter_id
+		LEFT JOIN mamba_flat_encounter_follow_up_4 mpf4
+			ON mpf.encounter_id = mpf4.encounter_id
+		LEFT JOIN mamba_flat_encounter_follow_up_6 mpf6
+			ON mpf.encounter_id = mpf6.encounter_id
+		LEFT JOIN mamba_flat_encounter_follow_up_7 mpf7
+			ON mpf.encounter_id = mpf7.encounter_id
+		LEFT JOIN mamba_flat_encounter_follow_up_8 mpf8
+			ON mpf.encounter_id = mpf8.encounter_id
+		WHERE REPORT_START_DATE is not null and REPORT_END_DATE is not null and (mpnf.follow_up_date BETWEEN REPORT_START_DATE and REPORT_END_DATE) or
+			(REPORT_START_DATE is not null and REPORT_END_DATE is null and (mpnf.follow_up_date >= REPORT_START_DATE)) or
+			(REPORT_START_DATE is null and REPORT_END_DATE is not null and (mpnf.follow_up_date <= REPORT_END_DATE)) or
             (REPORT_START_DATE is null and REPORT_END_DATE is null)
-    ),
-    provided_and_retuned AS (
-		SELECT
-			pc.client_id,
-            pc.followup_date,
-            pc.coupon_id,
-            pc.conventional_result,
-            mpi.identifier AS phrh_code_returning_coupon,
-            fn_gregorian_to_ethiopian_calendar(mpf.followup_date, 'Y-M-D') AS date_returned_ec,
-            mpf.followup_date as date_returned,
-            mpf.etb_paid_for_recruiter,
-            mpf.final_hiv_test_result AS conventional_result_returning
-		FROM provided_date pc
-        LEFT JOIN mamba_flat_encounter_phrh_followup mpf
-			ON mpf.coupon_id=pc.coupon_id
-		LEFT JOIN mamba_dim_patient_identifier mpi
-			ON mpf.client_id = mpi.patient_id and
-            mpi.identifier_type = (SELECT patient_identifier_type_id FROM mamba_dim_patient_identifier_type WHERE name='PHRH')
-    ),
-    provided_and_retuned_with_phrh AS (
-        SELECT
-            mps.*,
-            mpi.identifier AS phrh,
-            fn_gregorian_to_ethiopian_calendar(mps.followup_date, 'Y-M-D') AS date_provided_ec,
-            mps.followup_date AS date_provided
-        FROM provided_and_retuned mps
-        INNER JOIN mamba_dim_patient_identifier mpi
-            ON mps.client_id = mpi.patient_id
-        INNER JOIN mamba_dim_patient_identifier_type mpit
-            ON mpi.identifier_type = mpit.patient_identifier_type_id  and mpit.name = 'PHRH'
-		WHERE (PHRH_CODE is not null and mpi.identifier = PHRH_CODE) or PHRH_CODE is null
     )
+
     SELECT
-		prp.phrh AS 'PHRH Code Provided Coupon',
-        prp.coupon_id AS 'Coupon ID',
-        prp.date_provided_ec AS 'Date Provided (EC)',
-        prp.date_provided AS 'Date Provided (GC)',
-        prp.conventional_result AS 'Con Result Provided Coupon',
-        prp.phrh_code_returning_coupon AS 'PHRH Code Returning Coupon',
-        prp.date_returned_ec AS 'Date Returned (EC)',
-        prp.date_returned AS 'Date Returned (GC)',
-        prp.etb_paid_for_recruiter AS 'ETB paid for Recruiter',
-        prp.conventional_result_returning AS 'Con Test Result Returning Coupon'
-    FROM provided_and_retuned_with_phrh prp;
+		f.mrn AS 'MRN',
+        f.uan AS 'UAN',
+        f.full_name AS 'Full Name',
+        f.gender AS 'Sex',
+        f.hiv_confirmed_date_ec AS 'HIV Confirmed Date (EC)',
+        f.hiv_confirmed_date AS 'HIV Confirmed Date (GC)',
+        f.art_start_date_ec AS 'ART Start Date (EC)',
+        f.art_start_date AS 'ART Start Date (GC)',
+        f.screening_date AS 'Screening Date',
+        f.art_followup_date AS 'Follow-Up Date (ART)',
+        f.region AS 'Address: Region',
+        f.zone AS 'Address: Zone',
+        f.woreda AS 'Address: Woreda',
+        f.phone_number AS 'Address: Phone Number (Mobile)',
+        f.diagnosis_type AS 'Diagnosis Type',
+        f.diagnosis AS 'Diagnosis',
+        f.outcome AS 'Outcome',
+        f.next_appointment_date_ec AS 'Next Appointment Date (NCD) (EC)',
+        f.next_appointment_date AS 'Next Appointment Date (NCD) (GC)',
+        f.ncd_follow_up_date_ec AS 'Follow-Up Date NCD  (EC)',
+        f.ncd_follow_up_date AS 'Follow-Up Date NCD  (GC)',
+        f.art_followup_status AS 'Follow-Up Status (ART)',
+        f.age AS 'Age',
+        f.art_regimen AS 'ART Regimen',
+        f.art_regimen_dose AS 'ART Regimen Dose',
+        f.visit_type AS 'Visit Type',
+        f.risk_factors AS 'Risk Factors',
+        f.dm_symptoms AS 'Diabetes Melitus (DM) Symptoms',
+        f.hypertension_symptoms AS 'Hypertension (HTN) Symptoms',
+        f.bp1 AS 'BP1',
+        f.bp2 AS 'BP2',
+        f.bmi AS 'BMI',
+        f.fbs AS 'FBS',
+        f.fundoscopy AS 'Fundoscopy',
+        f.dm_complications AS 'DM Complications',
+        f.htn_complications AS 'HTN Complications',
+        f.adherence AS 'Adherence',
+        f.lifestyle_changes AS 'Lifestyle Changes'
+	FROM ncd_followup f
+	WHERE f.ncd_followup_row_num = 1;
 END //
 
 DELIMITER ;
