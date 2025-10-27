fb94369720b3fc7e56b20b068804a8aac1b961f8
samuel <saa2280@cumc.columbia.edu>
2025-10-27 17:11:35 +0300

    tb prev checker added

--- api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/TBPrevNumeratorDATIMCheckerReportsMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/TBPrevNumeratorDATIMCheckerReportsMamba.java
@@ -1,7 +1,7 @@
-package org.openmrs.module.mambaetl.reports.datim;
+package org.openmrs.module.mambaetl.reports.linelist;
 
 import org.openmrs.module.mambaetl.datasetdefinition.datim.HeaderDataSetDefinitionMamba;
-import org.openmrs.module.mambaetl.datasetdefinition.datim.tb_prev.TBPrevNumeratorDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.TBPrevNumeratorCheckerDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.EthiOhriUtil;
 import org.openmrs.module.mambaetl.helpers.reportOptions.TBPrevAggregationTypes;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
@@ -16,21 +16,21 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
-public class TBPrevNumeratorDATIMReportsMamba implements ReportManager {
+public class TBPrevNumeratorDATIMCheckerReportsMamba implements ReportManager {
 	
 	@Override
 	public String getUuid() {
-		return "a049528e-8a46-401f-8d19-ef0f3863db39";
+		return "e68a7c75-3402-43eb-912a-8cd1bc8bcd5f";
 	}
 	
 	@Override
 	public String getName() {
-		return "DATIM PREVENTION- TB_PREV(NUMERATOR)";
+		return "LINELIST- TB_PREV(NUMERATOR) Datim Checker";
 	}
 	
 	@Override
 	public String getDescription() {
-		return "TB_PREV(NUMERATOR) DATIM mamba report";
+		return "TB_PREV(NUMERATOR) DATIM Checker mamba report";
 	}
 	
 	@Override
@@ -52,30 +52,23 @@ public class TBPrevNumeratorDATIMReportsMamba implements ReportManager {
 		reportDefinition.addDataSetDefinition("DSD: TB_PREV(NUMERATOR)",
 		    EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
 		
-		TBPrevNumeratorDataSetDefinitionMamba tbPrevTotalNumeratorDataSetDefinitionMamba = new TBPrevNumeratorDataSetDefinitionMamba();
-		tbPrevTotalNumeratorDataSetDefinitionMamba.addParameters(getParameters());
-		tbPrevTotalNumeratorDataSetDefinitionMamba.setTbPrevAggregationTypes(TBPrevAggregationTypes.TOTAL);
-		tbPrevTotalNumeratorDataSetDefinitionMamba
+		TBPrevNumeratorCheckerDataSetDefinitionMamba tbPrevNumeratorCheckerDataSetDefinitionMamba = new TBPrevNumeratorCheckerDataSetDefinitionMamba();
+		tbPrevNumeratorCheckerDataSetDefinitionMamba.addParameters(getParameters());
+		tbPrevNumeratorCheckerDataSetDefinitionMamba.setTbPrevAggregationTypes(TBPrevAggregationTypes.DEBUG); // TBPrevAggregationTypes.PREV_ART is set to avoid NPE (actual logic in evaluator)
+		tbPrevNumeratorCheckerDataSetDefinitionMamba
 		        .setDescription("Among those who started a course of TPT in the previous reporting period, the number that completed a full course of therapy. (for continuous IPT programs, this includes the patients who have completed the first 6 months of isoniazid preventive therapy (IPT), or any other standard course of TPT such as 3 months of weekly isoniazid and rifapentine, or 3-HP)");
 		reportDefinition
 		        .addDataSetDefinition(
 		            "Among those who started a course of TPT in the previous reporting period, the number that completed a full course of therapy. (for continuous IPT programs, this includes the patients who have completed the first 6 months of isoniazid preventive therapy (IPT), or any other standard course of TPT such as 3 months of weekly isoniazid and rifapentine, or 3-HP)",
-		            EthiOhriUtil.map(tbPrevTotalNumeratorDataSetDefinitionMamba,
+		            EthiOhriUtil.map(tbPrevNumeratorCheckerDataSetDefinitionMamba,
 		                "startDate=${startDateGC},endDate=${endDateGC}"));
-		
-		TBPrevNumeratorDataSetDefinitionMamba tbPrevNumeratorDataSetDefinitionMamba = new TBPrevNumeratorDataSetDefinitionMamba();
-		tbPrevNumeratorDataSetDefinitionMamba.addParameters(getParameters());
-		tbPrevNumeratorDataSetDefinitionMamba.setTbPrevAggregationTypes(TBPrevAggregationTypes.PREV_ART); // TBPrevAggregationTypes.PREV_ART is set to avoid NPE (actual logic in evaluator)
-		tbPrevNumeratorDataSetDefinitionMamba.setDescription("Disaggregated By ART Start by Age/Sex");
-		reportDefinition.addDataSetDefinition("Disaggregated By ART Start by Age/Sex",
-		    EthiOhriUtil.map(tbPrevNumeratorDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
 		
 		return reportDefinition;
 	}
 	
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-		ReportDesign design = ReportManagerUtil.createExcelDesign("1a5cfec5-7f87-4c75-ba7c-2d9c40804d3b", reportDefinition);
+		ReportDesign design = ReportManagerUtil.createExcelDesign("7fbe039f-05f7-42ef-94bb-a08f50c2a435", reportDefinition);
 		design.setReportDefinition(reportDefinition);
 		return Collections.singletonList(design);
 	}
