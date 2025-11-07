48178da493fe3173d87e65e5a5bfd56e2f0a5b24
samuel <saa2280@cumc.columbia.edu>
2025-11-07 15:06:47 +0300

    ti status update

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_art_cohort_analysis_summary_query.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_art_cohort_analysis_summary_query.sql
@@ -213,8 +213,7 @@ BEGIN
                                                 OVER (PARTITION BY PatientId ORDER BY interval_month)             AS previous_initial_outcome,
                                             LAG(treatment_end_date, 1)
                                                 OVER (PARTITION BY PatientId ORDER BY interval_month)             AS previous_treatment_end_date,
-                                            LAG(regimen, 1) OVER (PARTITION BY PatientId ORDER BY interval_month) AS previous_regimen,
-                                            FIRST_VALUE(ti_status) OVER (PARTITION BY PatientId ORDER BY interval_month) AS initial_ti_status
+                                            LAG(regimen, 1) OVER (PARTITION BY PatientId ORDER BY interval_month) AS previous_regimen
                                      FROM CohortDetails_TMP),
 
          CohortDetails_With_Terminal_Flag AS (SELECT *,
@@ -267,13 +266,13 @@ BEGIN
                                          on client.client_id = CohortDetails_With_Last_Terminal_Status.PatientId)
 
     SELECT 'A. Started on ART in this clinic: original cohort'                            AS Name,
-           CAST(IFNULL(SUM(CASE WHEN interval_month = 0 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 0',
-           CAST(IFNULL(SUM(CASE WHEN interval_month = 0 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 6',
-           CAST(IFNULL(SUM(CASE WHEN interval_month = 0 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 12',
-           CAST(IFNULL(SUM(CASE WHEN interval_month = 0 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 24',
-           CAST(IFNULL(SUM(CASE WHEN interval_month = 0 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 36'
+           CAST(IFNULL(SUM(CASE WHEN interval_month = 36 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 0',
+           CAST(IFNULL(SUM(CASE WHEN interval_month = 36 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 6',
+           CAST(IFNULL(SUM(CASE WHEN interval_month = 36 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 12',
+           CAST(IFNULL(SUM(CASE WHEN interval_month = 36 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 24',
+           CAST(IFNULL(SUM(CASE WHEN interval_month = 36 THEN 1 ELSE 0 END), 0) AS SIGNED) AS 'Month 36'
     FROM CohortDetails
-    WHERE initial_ti_status IS NULL OR initial_ti_status NOT IN ('TI', 'Transferred In')
+    WHERE ti_status IS NULL OR ti_status NOT IN ('TI', 'Transferred In')
     UNION ALL
     SELECT 'B. Transfer in Add+'     AS Name,
            0                         AS 'Month 0',
