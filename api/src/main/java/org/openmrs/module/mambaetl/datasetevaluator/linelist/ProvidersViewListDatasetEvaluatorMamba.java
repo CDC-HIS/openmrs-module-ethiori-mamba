046563ce69c5db5a879fe1e4c45f6692df1455df
samuel <saa2280@cumc.columbia.edu>
2025-11-14 12:15:59 +0300

    date issue resolved

--- api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/ProvidersViewListDatasetEvaluatorMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/ProvidersViewListDatasetEvaluatorMamba.java
@@ -68,7 +68,7 @@ public class ProvidersViewListDatasetEvaluatorMamba implements DataSetEvaluator 
     }
 	
 	private List<ProcedureCall> createProcedureCalls(ProvidersViewLineListDataSetDefinitionMamba dataSetDefinitionMamba) {
-        java.sql.Date endDate = new java.sql.Date(dataSetDefinitionMamba.getEndDate().getTime());
+        java.sql.Date endDate = dataSetDefinitionMamba.getEndDate() != null ? new java.sql.Date(dataSetDefinitionMamba.getEndDate().getTime()) : null;
 
 
         return Collections.singletonList(
