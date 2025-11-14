5fa799380469976dee59abd1200ab67f174a8958
samuel <saa2280@cumc.columbia.edu>
2025-11-14 17:15:21 +0300

    tx tb

--- api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/tx_tb/TxTBNumeratorEvaluatorMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/tx_tb/TxTBNumeratorEvaluatorMamba.java
@@ -3,7 +3,6 @@ package org.openmrs.module.mambaetl.datasetevaluator.datim.tx_tb;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_tb.TxTBDenominatorDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.datasetdefinition.datim.tx_tb.TxTBNumeratorDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper;
 import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
