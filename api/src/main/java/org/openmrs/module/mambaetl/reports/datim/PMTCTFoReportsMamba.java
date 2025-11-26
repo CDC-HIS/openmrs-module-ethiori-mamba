924735a78902938348aaeacb050d907d61145fc6
samuel <saa2280@cumc.columbia.edu>
2025-11-26 16:35:06 +0300

    PMTCT Update

--- api/src/main/java/org/openmrs/module/mambaetl/reports/datim/PMTCTFoReportsMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/datim/PMTCTFoReportsMamba.java
@@ -14,30 +14,30 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 import java.util.List;
 
-//@Component
+@Component
 public class PMTCTFoReportsMamba implements ReportManager {
-	
+
 	@Override
 	public String getUuid() {
 		return "1f9de747-db69-4e79-960f-5fc15ff13ea5";
 	}
-	
+
 	@Override
 	public String getName() {
 		return "DATIM TREATMENT- PMTCT_FO";
 	}
-	
+
 	@Override
 	public String getDescription() {
 		return "PMTCT_FO DATIM mamba report ";
 	}
-	
+
 	@Override
 	public List<Parameter> getParameters() {
 		return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
-		
+
 	}
-	
+
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition reportDefinition = new ReportDefinition();
@@ -45,37 +45,49 @@ public class PMTCTFoReportsMamba implements ReportManager {
 		reportDefinition.setName(getName());
 		reportDefinition.setDescription(getDescription());
 		reportDefinition.setParameters(getParameters());
-		
+
 		HeaderDataSetDefinitionMamba headerDefinition = new HeaderDataSetDefinitionMamba();
 		headerDefinition.setDescription("DSD: PMTCT_FO");
 		headerDefinition.setParameters(getParameters());
-		reportDefinition.addDataSetDefinition("DSD: PMTCT_FO", EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
-		
-		PmtctFoDataSetDefinitionMamba pmtctFoDataSetDefinitionMamba = new PmtctFoDataSetDefinitionMamba();
-		pmtctFoDataSetDefinitionMamba.addParameters(getParameters());
-		pmtctFoDataSetDefinitionMamba.setDescription("Numerator: Disaggregated by Outcome Type");
+		reportDefinition.addDataSetDefinition("DSD: PMTCT_FO",
+				EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
+
+		// Denominator: Auto-Calculate
+		PmtctFoDataSetDefinitionMamba denominatorDataSet = new PmtctFoDataSetDefinitionMamba();
+		denominatorDataSet.addParameters(getParameters());
+		denominatorDataSet.setDescription("Auto-Calculate");
+		denominatorDataSet.setReportType("DENOMINATOR");
+		reportDefinition.addDataSetDefinition("Denominator",
+				EthiOhriUtil.map(denominatorDataSet, "startDate=${startDateGC},endDate=${endDateGC}"));
+
+		// Numerator: Disaggregated by Outcome Type
+		PmtctFoDataSetDefinitionMamba numeratorDataSet = new PmtctFoDataSetDefinitionMamba();
+		numeratorDataSet.addParameters(getParameters());
+		numeratorDataSet.setDescription("Numerator: Disaggregated by Outcome Type");
+		numeratorDataSet.setReportType("DISAGGREGATED");
 		reportDefinition.addDataSetDefinition("Numerator: Disaggregated by Outcome Type",
-		    EthiOhriUtil.map(pmtctFoDataSetDefinitionMamba, "startDate=${startDateGC},endDate=${endDateGC}"));
-		
+				EthiOhriUtil.map(numeratorDataSet, "startDate=${startDateGC},endDate=${endDateGC}"));
+
 		return reportDefinition;
 	}
-	
+
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-		
-		ReportDesign design = ReportManagerUtil.createExcelDesign("f89d69a0-9222-4d2f-88a1-8eafb82d48f3", reportDefinition);
+
+		ReportDesign design = ReportManagerUtil.createExcelDesign("f89d69a0-9222-4d2f-88a1-8eafb82d48f3",
+				reportDefinition);
 		design.setReportDefinition(reportDefinition);
 		return Collections.singletonList(design);
 	}
-	
+
 	@Override
 	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
 		return null;
 	}
-	
+
 	@Override
 	public String getVersion() {
 		return "1.0.0-SNAPSHOT";
 	}
-	
+
 }
