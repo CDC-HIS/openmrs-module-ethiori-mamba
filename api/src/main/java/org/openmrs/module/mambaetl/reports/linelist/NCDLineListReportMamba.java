ceda70fe4658cf87791ceb3f04d48553d1c9a1cb
samuel <saa2280@cumc.columbia.edu>
2025-11-26 16:37:50 +0300

    PMTCT Update

--- api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/NCDLineListReportMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/linelist/NCDLineListReportMamba.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 import java.util.List;
 
-//@Component
+@Component
 public class NCDLineListReportMamba implements ReportManager {
 	
 	@Override
