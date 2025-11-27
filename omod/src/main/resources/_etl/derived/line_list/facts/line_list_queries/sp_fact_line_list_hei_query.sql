d93dec286cfaeaed942f3ebb50af4288f852be28
samuel <saa2280@cumc.columbia.edu>
2025-11-27 16:30:27 +0300

    fix errors

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_hei_query.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_hei_query.sql
@@ -2,55 +2,12 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS sp_fact_line_list_hei_query;
 
-CREATE PROCEDURE sp_fact_line_list_hei_query(IN REPORT_START_DATE DATE, IN REPORT_END_DATE DATE)
+CREATE PROCEDURE sp_fact_line_list_hei_query(
+    IN REPORT_START_DATE DATE,
+    IN REPORT_END_DATE DATE
+)
 BEGIN
 
-    WITH Enrollment_tmp as (select client_id,
-                                   antenatal_care_provider,
-                                   ld_client,
-                                   post_natal_care,
-                                   art_clinic,
-                                   location_of_birth,
-                                   date_of_enrollment_or_booking,
-                                   ROW_NUMBER() over (PARTITION BY client_id ORDER BY date_of_enrollment_or_booking DESC, encounter_id DESC) as row_num
-                            from mamba_flat_encounter_pmtct_enrollment
-                            where date_of_enrollment_or_booking BETWEEN REPORT_START_DATE AND REPORT_END_DATE),
-         Enrollment as (select * from Enrollment_tmp where row_num = 1),
-         FollowUp AS (select follow_up.encounter_id,
-                             follow_up.client_id                 AS PatientId,
-                             follow_up_status,
-                             follow_up_date_followup_            AS follow_up_date,
-                             art_antiretroviral_start_date       AS art_start_date,
-                             assessment_date,
-                             treatment_end_date,
-                             antiretroviral_art_dispensed_dose_i AS ARTDoseDays,
-                             weight_text_                        AS Weight,
-                             screening_test_result_tuberculosis  AS TB_SreeningResult,
-                             date_of_last_menstrual_period_lmp_     LMP_Date,
-                             anitiretroviral_adherence_level     AS AdherenceLevel,
-                             next_visit_date,
-                             regimen,
-                             currently_breastfeeding_child          breast_feeding_status,
-                             pregnancy_status,
-                             diagnosis_date                      AS ActiveTBDiagnoseddate,
-                             nutritional_status_of_adult,
-                             nutritional_supplements_provided,
-                             stages_of_disclosure,
-                             date_started_on_tuberculosis_prophy,
-                             method_of_family_planning,
-                             patient_diagnosed_with_active_tuber as ActiveTBDiagnosed,
-                             dsd_category,
-                             nutritional_screening_result,
-                             cd4_count,
-                             date_of_event                       as hiv_confirmed_date,
-                             date_started_on_tuberculosis_prophy AS inhprophylaxis_started_date,
-                             date_completed_tuberculosis_prophyl AS InhprophylaxisCompletedDate,
-                             date_active_tbrx_completed,
-                             date_of_reported_hiv_viral_load     as viral_load_sent_date,
-                             date_viral_load_results_received    AS viral_load_perform_date,
-                             date_referred_to_pmtct,
-                             viral_load_test_status,
-                             CASE visitect_cd4_result
     -- HEI Line List Query
     -- Aggregates data from Enrollment, Follow-up, HIV Testing, and Final Outcome
 
@@ -76,12 +33,12 @@ BEGIN
             f.client_id,
             f.follow_up_date_followup_ as follow_up_date,
             f.weight_text as current_weight,
-            f.infant_feeding_practice_within_the_first_6_months_of_life,
-            f.infant_feeding_practice_older_than_6_months_of_life,
-            COALESCE(f.infant_feeding_practice_within_the_first_6_months_of_life, f.infant_feeding_practice_older_than_6_months_of_life) as feeding_practice,
-            f.cotrimoxazole_prophylaxis_dose,
-            f.cotrimoxazole_adherence_level,
-            f.developmental_milestone_for_children,
+#             f.infant_feeding_practice_within_the_first_6_months_of_life,
+#             f.infant_feeding_practice_older_than_6_months_of_life,
+#             COALESCE(f.infant_feeding_practice_within_the_first_6_months_of_life, f.infant_feeding_practice_older_than_6_months_of_life) as feeding_practice,
+#             f.cotrimoxazole_prophylaxis_dose,
+#             f.cotrimoxazole_adherence_level,
+#             f.developmental_milestone_for_children,
             f.next_visit_date,
             ROW_NUMBER() OVER (PARTITION BY f.client_id ORDER BY f.encounter_datetime DESC) as rn
         FROM mamba_flat_encounter_hei_followup f
@@ -141,9 +98,9 @@ BEGIN
         -- Follow Up
         f.follow_up_date as `Latest Follow-up Date`,
         f.current_weight as `Current Weight`,
-        f.feeding_practice as `Feeding Practice`,
-        f.cotrimoxazole_prophylaxis_dose as `CPT Dose`,
-        f.developmental_milestone_for_children as `Developmental Milestones`,
+#         f.feeding_practice as `Feeding Practice`,
+#         f.cotrimoxazole_prophylaxis_dose as `CPT Dose`,
+#         f.developmental_milestone_for_children as `Developmental Milestones`,
         f.next_visit_date as `Next Visit Date`,
         
         -- Final Outcome
