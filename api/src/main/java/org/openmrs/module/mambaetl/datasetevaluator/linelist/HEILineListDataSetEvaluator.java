49422e7753821fedac294e1e78537bec2a6f78d6
samuel <saa2280@cumc.columbia.edu>
2025-11-27 16:10:06 +0300

    Hei line list implemented

---
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/HEILineListDataSetEvaluator.java
@@ -0,0 +1,85 @@
+package org.openmrs.module.mambaetl.datasetevaluator.linelist;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.HEILineListDatasetDefinition;
+import org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper;
+import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
+import org.openmrs.module.reporting.dataset.DataSet;
+import org.openmrs.module.reporting.dataset.SimpleDataSet;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.evaluator.DataSetEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper.*;
+import static org.openmrs.module.mambaetl.helpers.ValidationHelper.ValidateDates;
+
+@Handler(supports = { HEILineListDatasetDefinition.class })
+public class HEILineListDataSetEvaluator implements DataSetEvaluator {
+
+    private static final Log log = LogFactory.getLog(HEILineListDataSetEvaluator.class);
+
+    private static final String ERROR_PROCESSING_RESULT_SET = "Error processing ResultSet: ";
+
+    private static final String DATABASE_CONNECTION_ERROR = "Database connection error: ";
+
+    @Override
+    public DataSet evaluate(DataSetDefinition dataSetDefinition, EvaluationContext evalContext)
+            throws EvaluationException {
+
+        HEILineListDatasetDefinition heiLineListDatasetDefinition = (HEILineListDatasetDefinition) dataSetDefinition;
+        SimpleDataSet data = new SimpleDataSet(dataSetDefinition, evalContext);
+        ResultSetMapper resultSetMapper = new ResultSetMapper();
+
+        ValidateDates(data, heiLineListDatasetDefinition.getStartDate(), heiLineListDatasetDefinition.getEndDate());
+        if (!data.getRows().isEmpty()) {
+            return data;
+        }
+        try (Connection connection = DataSetEvaluatorHelper.getDataSource().getConnection()) {
+            connection.setAutoCommit(false);
+
+            List<DataSetEvaluatorHelper.ProcedureCall> procedureCalls = createProcedureCalls(
+                    heiLineListDatasetDefinition);
+
+            try (DataSetEvaluatorHelper.CallableStatementContainer statementContainer = prepareStatements(connection,
+                    procedureCalls)) {
+
+                executeStatements(statementContainer, procedureCalls);
+
+                ResultSet[] allResultSets = statementContainer.getResultSets();
+                mapResultSet(data, resultSetMapper, allResultSets, Boolean.TRUE);
+                connection.commit();
+                return data;
+
+            } catch (SQLException e) {
+                rollbackAndThrowException(connection, ERROR_PROCESSING_RESULT_SET + e.getMessage(), e, log);
+            }
+        } catch (SQLException e) {
+            throw new EvaluationException(DATABASE_CONNECTION_ERROR + e.getMessage(), e);
+        }
+        return null;
+    }
+
+    private List<ProcedureCall> createProcedureCalls(HEILineListDatasetDefinition dataSetDefinitionMamba) {
+        java.sql.Date startDate = dataSetDefinitionMamba.getStartDate() != null
+                ? new java.sql.Date(dataSetDefinitionMamba.getStartDate().getTime())
+                : null;
+        java.sql.Date endDate = dataSetDefinitionMamba.getEndDate() != null
+                ? new java.sql.Date(dataSetDefinitionMamba.getEndDate().getTime())
+                : null;
+
+        return Collections.singletonList(
+                new ProcedureCall("{call sp_fact_line_list_hei_query(?,?)}", statement -> {
+                    statement.setDate(1, startDate);
+                    statement.setDate(2, endDate);
+                }));
+    }
+}
