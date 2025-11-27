44af91bd6359a9a34ec4c7ef1cc2ab322f16df17
samuel <saa2280@cumc.columbia.edu>
2025-11-27 14:14:23 +0300

    PMTCT datim

--- api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/pmtct/PmtctHeiEvaluatorMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/datim/pmtct/PmtctHeiEvaluatorMamba.java
@@ -3,7 +3,6 @@ package org.openmrs.module.mambaetl.datasetevaluator.datim.pmtct;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.mambaetl.datasetdefinition.datim.pmtct.PmtctFoDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.datasetdefinition.datim.pmtct.PmtctHeiDataSetDefinitionMamba;
 import org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper;
 import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
--- api/src/main/java/org/openmrs/module/mambaetl/reports/datim/PMTCTHeiReportsMamba.java
+++ api/src/main/java/org/openmrs/module/mambaetl/reports/datim/PMTCTHeiReportsMamba.java
@@ -17,28 +17,28 @@ import java.util.List;
 
 @Component
 public class PMTCTHeiReportsMamba implements ReportManager {
-
+	
 	@Override
 	public String getUuid() {
 		return "a5644908-8981-4904-973a-3eeaf6ccb234";
 	}
-
+	
 	@Override
 	public String getName() {
 		return "DATIM TREATMENT- PMTCT_HEI";
 	}
-
+	
 	@Override
 	public String getDescription() {
 		return "PMTCT_HEI DATIM mamba report ";
 	}
-
+	
 	@Override
 	public List<Parameter> getParameters() {
 		return EthiOhriUtil.getDateRangeParameters(Boolean.TRUE);
-
+		
 	}
-
+	
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition reportDefinition = new ReportDefinition();
@@ -46,52 +46,50 @@ public class PMTCTHeiReportsMamba implements ReportManager {
 		reportDefinition.setName(getName());
 		reportDefinition.setDescription(getDescription());
 		reportDefinition.setParameters(getParameters());
-
+		
 		HeaderDataSetDefinitionMamba headerDefinition = new HeaderDataSetDefinitionMamba();
 		headerDefinition.setDescription("DSD: PMTCT_HEI");
 		headerDefinition.setParameters(getParameters());
-		reportDefinition.addDataSetDefinition("DSD: PMTCT_HEI",
-				EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
-
+		reportDefinition.addDataSetDefinition("DSD: PMTCT_HEI", EthiOhriUtil.map(headerDefinition, "endDate=${endDateGC}"));
+		
 		// Numerator: Auto-Calculate
 		PmtctHeiDataSetDefinitionMamba numeratorDataSet = new PmtctHeiDataSetDefinitionMamba();
 		numeratorDataSet.addParameters(getParameters());
 		numeratorDataSet.setDescription("Auto-Calculate");
 		numeratorDataSet.setReportType("NUMERATOR");
 		reportDefinition.addDataSetDefinition("Numerator",
-				EthiOhriUtil.map(numeratorDataSet, "startDate=${startDateGC},endDate=${endDateGC}"));
-
+		    EthiOhriUtil.map(numeratorDataSet, "startDate=${startDateGC},endDate=${endDateGC}"));
+		
 		// Disaggregated by infant age at virologic sample collection and result
 		// returned
 		PmtctHeiDataSetDefinitionMamba disaggregatedDataSet = new PmtctHeiDataSetDefinitionMamba();
 		disaggregatedDataSet.addParameters(getParameters());
 		disaggregatedDataSet.setHeiAggregationTypes(HEIAggregationTypes.RESULT_COLLECTED);
 		disaggregatedDataSet
-				.setDescription("Disaggregated by infant age at virologic sample collection and result returned.");
+		        .setDescription("Disaggregated by infant age at virologic sample collection and result returned.");
 		disaggregatedDataSet.setReportType("DISAGGREGATED");
 		reportDefinition.addDataSetDefinition("Infants who had a first virologic HIV test (sample collected) by:",
-				EthiOhriUtil.map(disaggregatedDataSet, "startDate=${startDateGC},endDate=${endDateGC}"));
-
+		    EthiOhriUtil.map(disaggregatedDataSet, "startDate=${startDateGC},endDate=${endDateGC}"));
+		
 		return reportDefinition;
 	}
-
+	
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-
-		ReportDesign design = ReportManagerUtil.createExcelDesign("ded2a340-2b9c-4948-97c3-952d7b438aea",
-				reportDefinition);
+		
+		ReportDesign design = ReportManagerUtil.createExcelDesign("ded2a340-2b9c-4948-97c3-952d7b438aea", reportDefinition);
 		design.setReportDefinition(reportDefinition);
 		return Collections.singletonList(design);
 	}
-
+	
 	@Override
 	public List<ReportRequest> constructScheduledRequests(ReportDefinition reportDefinition) {
 		return null;
 	}
-
+	
 	@Override
 	public String getVersion() {
 		return "1.0.0-SNAPSHOT";
 	}
-
+	
 }
