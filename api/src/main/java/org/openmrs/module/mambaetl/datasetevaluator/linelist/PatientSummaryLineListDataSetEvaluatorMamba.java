913dfc9fda62c3bcd0699eaf56b79be70cb51ee8
samuel <saa2280@cumc.columbia.edu>
2025-12-29 09:31:48 +0300

    extract patient sp for patient summary

---
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/linelist/PatientSummaryLineListDataSetEvaluatorMamba.java
@@ -0,0 +1,76 @@
+package org.openmrs.module.mambaetl.datasetevaluator.linelist;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.mambaetl.datasetdefinition.linelist.PatientSummaryLineListDataSetDefinitionMamba;
+import org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper;
+import org.openmrs.module.mambaetl.helpers.mapper.ResultSetMapper;
+import org.openmrs.module.reporting.dataset.DataSet;
+import org.openmrs.module.reporting.dataset.SimpleDataSet;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.evaluator.DataSetEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper.*;
+
+@Handler(supports = { PatientSummaryLineListDataSetDefinitionMamba.class })
+public class PatientSummaryLineListDataSetEvaluatorMamba implements DataSetEvaluator {
+	
+	private static final Log log = LogFactory.getLog(PatientSummaryLineListDataSetEvaluatorMamba.class);
+	
+	private static final String ERROR_PROCESSING_RESULT_SET = "Error processing ResultSet: ";
+	
+	private static final String DATABASE_CONNECTION_ERROR = "Database connection error: ";
+	
+	@Override
+    public DataSet evaluate(DataSetDefinition dataSetDefinition, EvaluationContext evalContext)
+            throws EvaluationException {
+
+        PatientSummaryLineListDataSetDefinitionMamba dataSetDefinitionMamba = (PatientSummaryLineListDataSetDefinitionMamba) dataSetDefinition;
+        SimpleDataSet data = new SimpleDataSet(dataSetDefinition, evalContext);
+        ResultSetMapper resultSetMapper = new ResultSetMapper();
+
+        try (Connection connection = DataSetEvaluatorHelper.getDataSource().getConnection()) {
+            connection.setAutoCommit(false);
+
+            List<ProcedureCall> procedureCalls = createProcedureCalls(dataSetDefinitionMamba);
+
+            try (CallableStatementContainer statementContainer = prepareStatements(connection, procedureCalls)) {
+
+                executeStatements(statementContainer, procedureCalls);
+
+                ResultSet[] allResultSets = statementContainer.getResultSets();
+                mapResultSet(data, resultSetMapper, allResultSets, Boolean.FALSE);
+                connection.commit();
+                return data;
+
+            } catch (SQLException e) {
+                rollbackAndThrowException(connection, ERROR_PROCESSING_RESULT_SET + e.getMessage(), e, log);
+            }
+        } catch (SQLException e) {
+            throw new EvaluationException(DATABASE_CONNECTION_ERROR + e.getMessage(), e);
+        }
+        return null;
+    }
+	
+	private List<ProcedureCall> createProcedureCalls(PatientSummaryLineListDataSetDefinitionMamba dataSetDefinitionMamba) {
+
+
+        String procedureName = "{call sp_fact_line_list_patient_summary_query(?)}";
+
+        return Collections.singletonList(
+                new ProcedureCall(procedureName, statement -> {
+                    statement.setString(1, dataSetDefinitionMamba.getPatientUUID());
+                })
+        );
+    }
+}
