f01bd351f9f147f8119eaa4d11419bb77c9ebe7e
samuel <saa2280@cumc.columbia.edu>
2026-02-11 11:30:19 +0300

    Implementing API

---
+++ api/src/main/java/org/openmrs/module/mambaetl/api/db/impl/jdbcPatientSummaryDao.java
@@ -0,0 +1,55 @@
+package org.openmrs.module.mambaetl.api.db.impl;
+
+import org.openmrs.module.mambaetl.api.db.PatientSummaryDao;
+import org.openmrs.module.mambacore.db.ConnectionPoolManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class jdbcPatientSummaryDao implements PatientSummaryDao {
+	
+	private static final Logger log = LoggerFactory.getLogger(jdbcPatientSummaryDao.class);
+	
+	@Override
+    public Map<String, Object> getPatientSummary(String patientUuid) {
+
+        DataSource dataSource = ConnectionPoolManager.getInstance().getDefaultDataSource();
+
+        String sql = "SELECT " +
+                "  * " +
+                "FROM mamba_fact_client " +
+                "WHERE patient_uuid = ? " +
+                "LIMIT 1";
+
+        Map<String, Object> result = null;
+
+        try (Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            // Bind the String UUID
+            statement.setString(1, patientUuid);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    result = new HashMap<>();
+                    ResultSetMetaData metaData = resultSet.getMetaData();
+                    int columnCount = metaData.getColumnCount();
+
+                    for (int i = 1; i <= columnCount; i++) {
+                        result.put(metaData.getColumnLabel(i), resultSet.getObject(i));
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            log.error("Failed to fetch patient summary for UUID: " + patientUuid, e);
+            throw new RuntimeException("Database error in Patient Summary API", e);
+        }
+
+        return result;
+    }
+}
---
+++ api/src/main/java/org/openmrs/module/mambaetl/web/PatientSummaryResource.java
@@ -0,0 +1,50 @@
+package org.openmrs.module.mambaetl.web;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.mambaetl.api.PatientSummaryService;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
+import org.openmrs.module.webservices.rest.web.resource.impl.EmptySearchResult;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import java.util.Arrays;
+import java.util.Map;
+
+// URL will be: /openmrs/ws/rest/v1/ethiohri/patientsummary
+@Resource(name = RestConstants.VERSION_1 + "/ethiohri/patientsummary", supportedClass = Object.class, supportedOpenmrsVersions = { "2.0 - 9.*" })
+public class PatientSummaryResource implements Searchable {
+	
+	private PatientSummaryService getService() {
+		return Context.getService(PatientSummaryService.class);
+	}
+	
+	@Override
+	public SimpleObject search(RequestContext context) throws ResponseException {
+		String uuid = context.getRequest().getParameter("patientUuid");
+		
+		if (uuid == null || uuid.isEmpty()) {
+			return new SimpleObject().add("error", "Parameter 'patientUuid' is required");
+		}
+		
+		try {
+			Map<String, Object> summary = getService().getPatientSummary(uuid);
+			
+			if (summary == null) {
+				return new EmptySearchResult().toSimpleObject(null);
+			}
+			
+			return new SimpleObject().add("results", Arrays.asList(summary));
+			
+		}
+		catch (IllegalArgumentException e) {
+			return new SimpleObject().add("error", e.getMessage());
+		}
+	}
+	
+	@Override
+	public String getUri(Object instance) {
+		return null;
+	}
+}
