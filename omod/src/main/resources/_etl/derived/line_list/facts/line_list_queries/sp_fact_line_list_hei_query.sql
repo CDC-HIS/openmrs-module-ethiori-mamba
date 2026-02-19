06772804ac48ca62da8799c8c843474465d84cdc
samuelabebayehu <saa2280@cumc.columbia.edu>
2026-02-19 20:39:56 +0300

    add columns to hei

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_hei_query.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_hei_query.sql
@@ -15,6 +15,8 @@ BEGIN
             e.date_enrolled_in_care as enrollment_date,
             e.hei_code,
             e.mothers_name,
+            infant_referred,
+            mother_s_pmtct_interventions,
             e.mother_status,
             e.arv_prophylaxis as enrollment_arv_prophylaxis,
             e.weight_text as birth_weight,
@@ -27,17 +29,23 @@ BEGIN
     LatestFollowUp AS (
         SELECT
             f.client_id,
+            f.growth_pattern,
+            f.delayed_milestones,
+            f.reason_for_growth_failure,
+            COALESCE(mother_s_breast_condition,mothers_breast_condition) as mothers_breast_condition,
+            f.reason_for_red_flag,
             f.followup_date_followup as follow_up_date,
             f.weight_text as current_weight,
-             f.infant_feeding_practice_within_the_first_6_months_of_life,
-            f.infant_feeding_practice_older_than_6_months_of_life,
-             COALESCE(f.infant_feeding_practice_within_the_first_6_months_of_life, f.infant_feeding_practice_older_than_6_months_of_life) as feeding_practice,
-             f.cotrimoxazole_prophylaxis_dose,
-             f.cotrimoxazole_adherence_level,
-             f.developmental_milestone_for_children,
+             f1.infant_feeding_practice_within_the_first_6_months_of_life,
+            f1.infant_feeding_practice_older_than_6_months_of_life,
+             COALESCE(f1.infant_feeding_practice_within_the_first_6_months_of_life, f1.infant_feeding_practice_older_than_6_months_of_life) as feeding_practice,
+             f1.cotrimoxazole_prophylaxis_dose,
+             f1.cotrimoxazole_adherence_level,
+             f1.developmental_milestone_for_children,
             f.next_visit_date,
             ROW_NUMBER() OVER (PARTITION BY f.client_id ORDER BY f.followup_date_followup DESC) as rn
         FROM mamba_flat_encounter_hei_followup f
+        left join mamba_flat_encounter_hei_followup_1 f1 on f.encounter_id=f1.encounter_id
         WHERE f.followup_date_followup <= REPORT_END_DATE
     ),
 
@@ -72,6 +80,9 @@ BEGIN
         c.patient_name as `Patient Name`,
         c.mrn as `MRN`,
         e.hei_code as `HEI Code`,
+        e.infant_referred as `Infant Referred?`,
+        e.referring_facility_name as `Referring Facility Name`,
+        e.mother_s_pmtct_interventions as `Mother''s PMTCT Intervention`,
         TIMESTAMPDIFF(MONTH, c.date_of_birth, REPORT_END_DATE) as `Age (Months)`,
         c.date_of_birth as `DOB`,
         c.sex as `Sex`,
@@ -81,22 +92,26 @@ BEGIN
         e.mothers_name as `Mother's Name`,
         e.mother_status as `Mother's Status`,
         e.enrollment_arv_prophylaxis as `ARV Prophylaxis at Enrollment`,
-        e.birth_weight as `Birth Weight`,
+        e.birth_weight as `Birth Weight (in gram)`,
         
         -- HIV Testing
-        t.pcr_1_date as `PCR 1 Date`,
-        t.pcr_1_result as `PCR 1 Result`,
-        t.pcr_2_date as `PCR 2 Date`,
-        t.pcr_2_result as `PCR 2 Result`,
+        t.pcr_1_date as `DNA-PCR Sample Collection Date1`,
+        t.pcr_1_result as `DNA-PCR Result 1`,
+        t.pcr_2_date as `DNA-PCR Sample Collection Date 2`,
+        t.pcr_2_result as `DNA-PCR Result 2`,
         t.antibody_test_date as `Antibody Test Date`,
         t.antibody_test_result as `Antibody Test Result`,
         
         -- Follow Up
         f.follow_up_date as `Latest Follow-up Date`,
         f.current_weight as `Current Weight`,
-         f.feeding_practice as `Feeding Practice`,
+        f.growth_pattern as `Growth Pattern`,
+        f.reason_for_growth_failure as `Reason for Growth Failure`,
+         f.feeding_practice as `Infant Feeding Practice`,
+         f.mothers_breast_condition as `Mothers Breast Condition`,
          f.cotrimoxazole_prophylaxis_dose as `CPT Dose`,
-         f.developmental_milestone_for_children as `Developmental Milestones`,
+         f.developmental_milestone_for_children as `Development Milestone`,
+         f.reason_for_red_flag as `Reason for Red Flag`,
         f.next_visit_date as `Next Visit Date`,
         
         -- Final Outcome
