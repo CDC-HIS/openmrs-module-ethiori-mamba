787f3f7e8f1b485a4a1639bfce5dde47a930de2a
samuelabebayehu <saa2280@cumc.columbia.edu>
2026-04-02 13:22:14 +0300

    Debug Queries.sql updated

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/Debug Queries.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/Debug Queries.sql
@@ -3,4 +3,4 @@ mysqladmin -u root -p extended -r -i 10 | grep Handler_read_rnd_next
 
 mysql  -u root -p$'Abcd@1234' -e "SHOW ENGINE INNODB STATUS;"| grep -n2 "$thread_id"| grep "undo log entries"
 
-mysql -u root -p$'Abcd@1234' -e "SHOW ENGINE INNODB STATUS;" | grep ", undo log entries" | sed -n 's/.*undo log entries \([0-9]*\).*/\1/p'
+mysql -u openmrs -p$'Pd123#@!' -e "SHOW ENGINE INNODB STATUS;" | grep ", undo log entries" | sed -n 's/.*undo log entries \([0-9]*\).*/\1/p'
