b51673f397f4fd798565cb538cbf06d55bba464d
samuel <saa2280@cumc.columbia.edu>
2026-04-08 15:40:57 +0300

    Removed KP

--- omod/src/main/resources/_etl/derived/datim/sp_dim_tx_new_datim_kp_query.sql
+++ omod/src/main/resources/_etl/derived/datim/sp_dim_tx_new_datim_kp_query.sql
@@ -6,12 +6,6 @@ CREATE PROCEDURE sp_dim_tx_new_datim_kp_query()
 BEGIN
 SELECT 'PWID' AS `Population Type`, 0 AS `Total`
 UNION ALL
-SELECT 'MSM', 0
-UNION ALL
-SELECT 'Transgender People', 0
-UNION ALL
-SELECT 'FSW', 0
-UNION ALL
 SELECT 'People in prison and other closed settings', 0;
 
 END //
