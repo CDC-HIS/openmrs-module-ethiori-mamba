83b87b2ae78fcc94cd8e819fb5f361a3fe2326dd
samuel <saa2280@cumc.columbia.edu>
2026-04-22 08:18:57 +0300

    prep fix

--- omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_pre_exposure_query.sql
+++ omod/src/main/resources/_etl/derived/line_list/facts/line_list_queries/sp_fact_line_list_pre_exposure_query.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS sp_fact_line_list_pre_exposure_query;
-CREATE PROCEDURE sp_fact_line_list_pre_exposure_query( IN REPORT_END_DATE DATE)
+CREATE PROCEDURE sp_fact_line_list_pre_exposure_query(IN REPORT_END_DATE DATE)
 BEGIN
     WITH PreExposure AS (select screening.client_id,
                                 screening.encounter_id,
@@ -10,29 +10,31 @@ BEGIN
                                 screening.type_of_client,
                                 screening_1.treatment_start_date,
                                 screening_1.do_you_have_an_hiv_positive_partner,
-                                screening_1.antiretroviral_art_dispensed_dose_in_days as screened_dose,
-                                follow_up.pregnancy_status                            as f_pregnancy_status,
+                                screening_1.antiretroviral_art_dispensed_dose_in_days                         as screened_dose,
+                                follow_up.pregnancy_status                                                    as f_pregnancy_status,
                                 follow_up_1.prep_dose_end_date,
                                 screening.screening_date,
                                 final_hiv_test_result,
-                                prep_antiretrovirals_prescribed           as regimen_screened,
-                                screening.prep_dose_end_date                          as screened_dose_end_date,
+                                prep_antiretrovirals_prescribed                                               as regimen_screened,
+                                screening.prep_dose_end_date                                                  as screened_dose_end_date,
                                 currently_breastfeeding_child,
-                                preexposure_prophylaxis_prep_regimen as regimen,
+                                preexposure_prophylaxis_prep_regimen                                          as regimen,
                                 referred_from,
                                 service_delivery_point_number,
-                                followup_date_followup_1             as follow_up_date,
+                                followup_date_followup_1                                                      as follow_up_date,
                                 screened_for_tuberculosis,
                                 screening_1.screen_result_for_sti,
-                                follow_up_1.chronic_kidney_disease_stage              AS EGFR,
+                                follow_up_1.chronic_kidney_disease_stage                                      AS EGFR,
                                 contraindications_to_prep_medicines,
                                 preexposure_prophylaxis_provision_site,
-                                reason_for_stopping_adherence             as reason_for_stopping,
+                                reason_for_stopping_adherence                                                 as reason_for_stopping,
                                 follow_up_1.method_of_family_planning,
-                                hiv_side_effects                          as side_effects,
+                                hiv_side_effects                                                              as side_effects,
                                 next_visit_date,
                                 number_of_missed_tablets_in_the_past_30_days,
-                                follow_up_status    as follow_up_status
+                                CASE
+                                    WHEN follow_up_status = 'Alive' THEN 'On PrEP'
+                                    ELSE follow_up_status END                                                 as follow_up_status
                          FROM mamba_flat_encounter_pre_exposure_scree screening
                                   left join mamba_flat_encounter_pre_exposure_scree_1 screening_1
                                             on screening.encounter_id = screening_1.encounter_id
@@ -93,50 +95,50 @@ BEGIN
 
 
     SELECT ROW_NUMBER() OVER (ORDER BY patient_name)           as `#`,
-           patient_uuid                                 as `Patient GUID`,
-           patient_name                                 as `Patient Name`,
-           CAST(mrn AS CHAR(20))                        as MRN,
-           uan                                          AS UAN,
+           patient_uuid                                        as `Patient GUID`,
+           patient_name                                        as `Patient Name`,
+           CAST(mrn AS CHAR(20))                               as MRN,
+           uan                                                 AS UAN,
 
            TIMESTAMPDIFF(YEAR, date_of_birth,
                          COALESCE(REPORT_END_DATE, CURDATE())) as `Age`,
-           sex                                          as Sex,
-           prep.service_delivery_point_number                as `UIC`,
-           prep.screening_date                               as `PrEP Screening Date in EC.`,
-           prep.screening_date                               as `PrEP Screening Date in GC.`,
-           prep.prep_started                                 as `PrEP Started?`,
-           prep.treatment_start_date                         as `PrEP Start Date in EC.`,
-           prep.treatment_start_date                         as `PrEP Start Date in G.C`,
-           prep.type_of_client                               as `Type of Client`,
-           prep.follow_up_date                               as `PrEP Follow Up Date in EC.`,
-           prep.follow_up_date                               as `PrEP Follow Up Date in GC.`,
-           prep.follow_up_status                             as `PrEP Follow Up Status`,
+           sex                                                 as Sex,
+           prep.service_delivery_point_number                  as `UIC`,
+           prep.screening_date                                 as `PrEP Screening Date in EC.`,
+           prep.screening_date                                 as `PrEP Screening Date in GC.`,
+           prep.prep_started                                   as `PrEP Started?`,
+           prep.treatment_start_date                           as `PrEP Start Date in EC.`,
+           prep.treatment_start_date                           as `PrEP Start Date in G.C`,
+           prep.type_of_client                                 as `Type of Client`,
+           prep.follow_up_date                                 as `PrEP Follow Up Date in EC.`,
+           prep.follow_up_date                                 as `PrEP Follow Up Date in GC.`,
+           prep.follow_up_status                               as `PrEP Follow Up Status`,
            case
                when prep.regimen is not null then prep.regimen
-               else prep.regimen_screened end                as `PrEP Regimen`,
-           prep.number_of_missed_tablets_in_the_past_30_days as `Missed Tablets`,
-           prep.next_visit_date                              as `Next Visit Date in EC.`,
-           prep.next_visit_date                              as `Next Visit Date in GC.`,
+               else prep.regimen_screened end                  as `PrEP Regimen`,
+           prep.number_of_missed_tablets_in_the_past_30_days   as `Missed Tablets`,
+           prep.next_visit_date                                as `Next Visit Date in EC.`,
+           prep.next_visit_date                                as `Next Visit Date in GC.`,
            case
                when prep.prep_dose_end_date is not null then prep.screened_dose_end_date
-               else prep.screened_dose end                   as `Dose End Date in EC.`,
+               else prep.screened_dose end                     as `Dose End Date in EC.`,
            case
                when prep.prep_dose_end_date is not null then prep.screened_dose_end_date
-               else prep.screened_dose end                   as `Dose End Date in GC.`,
-           prep.final_hiv_test_result                        as `HIV Test Final Result`,
+               else prep.screened_dose end                     as `Dose End Date in GC.`,
+           prep.final_hiv_test_result                          as `HIV Test Final Result`,
            prep.f_pregnancy_status                             as `Pregnant?`,
-           prep.currently_breastfeeding_child                as `Breastfeeding?`,
-           prep.method_of_family_planning                    as `Family Planning Method`,
-           prep.screened_for_tuberculosis                    as `TB Screening Result`,
-           prep.screen_result_for_sti                        as `STI Screening Result`,
-           prep.EGFR                                         as `eGFR Estimate`,
-           prep.side_effects                                 as `Side Effects`,
-           prep.reason_for_stopping                          as `Reason to stop PrEP`,
-           prep.contraindications_to_prep_medicines          as `Contraindications to PrEP Medicines`,
-           prep.sex_worker                                   as `Self-Identifying WECSW`,
-           prep.do_you_have_an_hiv_positive_partner          as `Have HIV+ Partner?`,
-           prep.preexposure_prophylaxis_provision_site       as `PrEP Provision Site`,
-           prep.referred_from                                as `Referred From`
+           prep.currently_breastfeeding_child                  as `Breastfeeding?`,
+           prep.method_of_family_planning                      as `Family Planning Method`,
+           prep.screened_for_tuberculosis                      as `TB Screening Result`,
+           prep.screen_result_for_sti                          as `STI Screening Result`,
+           prep.EGFR                                           as `eGFR Estimate`,
+           prep.side_effects                                   as `Side Effects`,
+           prep.reason_for_stopping                            as `Reason to stop PrEP`,
+           prep.contraindications_to_prep_medicines            as `Contraindications to PrEP Medicines`,
+           prep.sex_worker                                     as `Self-Identifying WECSW`,
+           prep.do_you_have_an_hiv_positive_partner            as `Have HIV+ Partner?`,
+           prep.preexposure_prophylaxis_provision_site         as `PrEP Provision Site`,
+           prep.referred_from                                  as `Referred From`
 
     FROM prep;
 
