f90565789b7ff907c7d2f6e3bfa7c8f5280e528c
samuel <saa2280@cumc.columbia.edu>
2026-04-22 10:09:01 +0300

    dhis 2 reporting fix

--- api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/hmis_dhis2/HMISDHIS2DataSetEvaluator.java
+++ api/src/main/java/org/openmrs/module/mambaetl/datasetevaluator/hmis_dhis2/HMISDHIS2DataSetEvaluator.java
@@ -22,6 +22,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
+import org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper.ProcedureCall;
 import static org.openmrs.module.mambaetl.helpers.DataSetEvaluatorHelper.rollbackAndThrowException;
 
 @Handler(supports = { HMISDHIS2DatasetDefinition.class })
